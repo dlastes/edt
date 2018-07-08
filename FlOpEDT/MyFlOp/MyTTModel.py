7851ed9745aea175dc7bdf4aed21a21ec25162d5
Pablo <pablo.seban@univ-tlse2.fr>
2018-07-08 12:25:53 +0200

    comment

--- FlOpEDT/MyFlOp/MyTTModel.py
+++ FlOpEDT/MyFlOp/MyTTModel.py
@@ -36,6 +36,7 @@ class MyTTModel(TTModel):
 
         # Minimize the number of busy days for tutors
         # (if it does not overcome the bound expressed in pref_slots_per_day)
+        # It should be stored in the database
         for i in self.wdb.instructors:
             slot_by_day_cost = 0
             # need to be sorted
