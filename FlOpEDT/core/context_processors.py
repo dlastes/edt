b7bca70a63ce38ffd52f910834b74f2753ea2bfe
ricardeau <rb.web@laposte.net>
2018-10-30 19:22:29 +0100

    return an empty value when department has not been initialized

--- FlOpEDT/core/context_processors.py
+++ FlOpEDT/core/context_processors.py
@@ -2,5 +2,9 @@ from django.conf import settings
 
 
 def edt_context(request):
+    print("---------------" + str(request))
     if hasattr(request, 'department'):
+        print("------------ssssss---" + str(request))
         return {'department': request.department.abbrev}
+    else:
+        return {}
