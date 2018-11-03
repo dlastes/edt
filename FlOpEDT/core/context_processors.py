6bf8f106ad4b711a60948efaf38a77c0ee5cf7fc
ricardeau <rb.web@laposte.net>
2018-11-03 16:27:08 +0100

    remove debug messages

--- FlOpEDT/core/context_processors.py
+++ FlOpEDT/core/context_processors.py
@@ -2,9 +2,7 @@ from django.conf import settings
 
 
 def edt_context(request):
-    print("---------------" + str(request))
     if hasattr(request, 'department'):
-        print("------------ssssss---" + str(request))
         return {'department': request.department.abbrev}
     else:
         return {}
