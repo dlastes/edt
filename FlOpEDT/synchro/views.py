7573f37a74844e91c9bee83a8bad1f29603c78e7
yahn <yahn.formanczak@gmail.com>
2018-11-10 15:50:41 +0100

    Amelioration sur le contenu des evenements ical

--- FlOpEDT/synchro/views.py
+++ FlOpEDT/synchro/views.py
@@ -8,34 +8,49 @@ from datetime import timedelta
 
 tz='Europe/Paris'
 
+
 def index(request):
-    context = { 'courses': get_course_list() }
-    return render(request, 'templates/synchro/index.html', context)
+    enseignant_list = []#TODO is_active=True, is_tutor=True
+    groupe_list = []#TODO basic=True
+    salle_list = []#TODO
+    context = { 'enseignants': enseignant_list, 'groupes':groupe_list, 'salles':salle_list }
+    return render(request, 'index.html', context)
+
 
 def enseignant(request, id):
     events=[]
     for c in get_course_list().filter(cours__tutor__username=id):
         e = create_event(c)
-        c.title = 'TODO'#TODO
+        e['title'] = c.cours.module.abbrev + ' ' + c.cours.type.name + ' - ' + c.cours.groupe.train_prog.abbrev + ' ' + c.cours.groupe.nom
         events.append(e)
     return render(request, 'ical.ics', {'events':events, 'timezone':tz})
 
+
 def groupe(request, promo_id, groupe_id):
-    context = { 'courses': get_course_list().filter(cours__groupe__basic=True, cours__groupe__nom=groupe_id, cours__groupe__train_prog__abbrev=promo_id) }
-    return render(request, 'ical.ics', context)
+    events=[]
+    for c in get_course_list().filter(cours__groupe__basic=True, cours__groupe__nom=groupe_id, cours__groupe__train_prog__abbrev=promo_id):
+        e = create_event(c)
+        e['title'] = c.cours.module.abbrev + ' ' + c.cours.type.name + ' - ' + c.cours.tutor.username
+        events.append(e)
+    return render(request, 'ical.ics', {'events':events, 'timezone':tz})
+
 
 def salle(request, id):
-    context = { 'courses': get_course_list().filter(room__name=id) }
-    return render(request, 'ical.ics', context)
+    events=[]
+    for c in  get_course_list().filter(room__name=id):
+        e = create_event(c)
+        events.append(e)
+    return render(request, 'ical.ics', {'events':events, 'timezone':tz})
 
 
 def get_course_list():
     return ScheduledCourse.objects.filter(copie_travail=0).order_by('cours__an', 'cours__semaine', 'creneau__jour_id', 'creneau__heure')
 
+
 def create_event(c):
-    p = str(c.cours.an) + '-W' + str(c.cours.semaine) + '-w' + str(c.creneau.jour_id) + ' 00:00'
+    p = str(c.cours.an) + '-W' + str(c.cours.semaine) + '-w' + str(c.creneau.jour_id) + ' ' + str(c.creneau.heure.hours) + ':'+ str(c.creneau.heure.minutes)
     begin = datetime.strptime(p, '%Y-W%W-w%w %H:%M')
-    end = begin + timedelta(minutes=90)#TODO
+    end = begin + timedelta(minutes=c.creneau.duration)
     return {'id':c.id,
          'title': c.cours.module.abbrev + ' ' + c.cours.type.name + ' - ' + c.cours.groupe.train_prog.abbrev + ' ' + c.cours.groupe.nom + ' - ' + c.cours.tutor.username,
          'location': c.room.name,
