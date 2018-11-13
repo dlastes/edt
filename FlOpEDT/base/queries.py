a2bd8590de2ee204675a7ef478b9ba67ed945b65
ricardeau <rb.web@laposte.net>
2018-11-13 11:11:55 +0100

    Update CourseType on create_first_department

--- FlOpEDT/base/queries.py
+++ FlOpEDT/base/queries.py
@@ -31,7 +31,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from base.models import Group, TrainingProgramme, GroupDisplay, \
     TrainingProgrammeDisplay, ScheduledCourse, EdtVersion, Department, Regen
 
-from base.models import Room, RoomType, RoomGroup, RoomSort, Period
+from base.models import Room, RoomType, RoomGroup, RoomSort, Period, CourseType
 from people.models import Tutor
 from TTapp.models import TTConstraint
 
@@ -43,7 +43,7 @@ def create_first_department():
     department = Department.objects.create(name="Default Department", abbrev="default")
     
     # Update all existing department related models
-    models = [TrainingProgramme, EdtVersion, Regen, RoomType, Period]
+    models = [TrainingProgramme, EdtVersion, Regen, RoomType, Period, CourseType]
     for model in models:
         model.objects.all().update(department=department)
 
