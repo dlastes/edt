65b494342a9293581c474224bfcfcf860c0505a3
ricardeau <rb.web@laposte.net>
2018-12-01 11:02:25 +0100

    Extrct PeriodWeeks class from module inner methods

--- FlOpEDT/base/tests/test_statistics.py
+++ FlOpEDT/base/tests/test_statistics.py
@@ -4,8 +4,10 @@ from django.core.management import call_command
 import base.models as models
 from base.core.statistics import    get_room_activity_by_day, \
                                     get_holidays_weeks, \
-                                    get_holiday_list, \
-                                    get_period
+                                    get_holiday_list
+
+from base.core.period_weeks import PeriodWeeks
+
 
 class StatisticsRoomTestCase(TestCase):
 
@@ -21,7 +23,7 @@ class StatisticsRoomTestCase(TestCase):
         self.assertTrue(True)
 
     def test_get_holidays_weeks(self):
-        period = get_period(2018)
+        period = PeriodWeeks(2018)
         holidays = list(get_holidays_weeks(period))
         self.assertIn(44, holidays)
 
@@ -29,16 +31,8 @@ class StatisticsRoomTestCase(TestCase):
         # May, 1, 2019
         wednesday, _ = models.Day.objects.get_or_create(day=models.Day.WEDNESDAY)
         holiday, _ = models.Holiday.objects.get_or_create(year=2019, week=19, day=wednesday)
-        period = get_period(2018)
+        period = PeriodWeeks(2018)
         holiday_list = list(get_holiday_list(period))
         self.assertIn((holiday.year, holiday.week, holiday.day.no), holiday_list)
-
-    def test_get_period_2018(self):
-        period_2018 = (
-            (2018, (35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52)),
-            (2019, (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26)))
-        
-        period = get_period(2018)
-        self.assertTupleEqual(period, period_2018)
 
     
