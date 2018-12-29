e1eea99c1318863b620f059150fc2dc752e4ec43
Pablo <pablo.seban@univ-tlse2.fr>
2018-12-29 06:27:12 +0100

    debug in extract planif file

--- FlOpEDT/misc/deploy_database/extract_planif_file.py
+++ FlOpEDT/misc/deploy_database/extract_planif_file.py
@@ -154,19 +154,19 @@ def ReadPlanifWeek(department, book, feuille, semaine, an):
                 for sp in SUPP_TUTORS:
                     C.supp_tutor.add(sp)
                     C.save()
-                for after_type in [x for x in comments + local_comments if x[0] == 'A']:
-                    try:
-                        n = int(after_type[1])
-                        s = 2
-                    except:
-                        n = 1
-                        s = 1
-                    course_type = after_type[s:]
-                    courses = Course.objects.filter(type__name=course_type, module=MODULE, semaine=semaine, an=an,
-                                                   groupe__in = list(GROUPE.ancestor_groups()) + [GROUPE])
-                    for course in courses[:n]:
-                        P = Dependency(cours1=course, cours2=C)
-                        P.save()
+            for after_type in [x for x in comments + local_comments if x[0] == 'A']:
+                try:
+                    n = int(after_type[1])
+                    s = 2
+                except ValueError:
+                    n = 1
+                    s = 1
+                course_type = after_type[s:]
+                courses = Course.objects.filter(type__name=course_type, module=MODULE, semaine=semaine, an=an,
+                                               groupe__in = list(GROUPE.ancestor_groups()) + [GROUPE])
+                for course in courses[:n]:
+                    P = Dependency(cours1=course, cours2=C)
+                    P.save()
 
             if 'D' in comments or 'D' in local_comments  and N >= 2:
                 for GROUPE in GROUPS:
