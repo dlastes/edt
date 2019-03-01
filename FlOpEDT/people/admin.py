84c30505114b24675990dedef5c5d84da3f08fee
alban <alban.t.47@gmail.com>
2019-03-01 11:30:35 +0100

    Add de script.js dans people et modif de admin

--- FlOpEDT/people/admin.py
+++ FlOpEDT/people/admin.py
@@ -4,7 +4,7 @@
 from django.contrib import admin
 from base.admin import DepartmentModelAdmin
 
-from people.models import FullStaff, SupplyStaff, BIATOS, Tutor, StudentPreferences
+from people.models import *
 
 from import_export import resources, fields
 
@@ -26,11 +26,18 @@ class TutorResource(resources.ModelResource):
 		model = Tutor
 		fields = ( "username", "first_name", "last_name", "email" )
 
-class StudentPreferences(resources.ModelResource):
+class StudentPreferencesResource(resources.ModelResource):
 
     class Meta:
-		model = Tutor
-		fields = ( "username", "first_name", "last_name", "email" )
+        model = StudentPreferences
+        fields = ( "student.username", "student.belong_to", "morning_weight", "free_half_day_weight" )
+
+
+class GroupPreferencesResource(resources.ModelResource):
+
+    class Meta:
+        model = GroupPreferences
+        fields = ( "group.name", "morning_weight", "free_half_day_weight" )
 
 
 admin.site.register(FullStaff, TutorModelAdmin)
