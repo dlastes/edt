b323fa43735d670283bd4fc76c7be76b2831e477
PRG <prenaud@irit.fr>
2019-04-03 23:51:58 +0200

    fixed Gaetan's bug: drag end when no start

--- FlOpEDT/base/static/base/creation.js
+++ FlOpEDT/base/static/base/creation.js
@@ -1282,6 +1282,9 @@ function def_drag() {
         })
 	.on("end", function(d) {
 
+            // click => end. So if real drag
+            if(drag.sel != null) {
+
             // lower the course to the middleground layer
             mg.node().appendChild(drag.sel.node());
 
@@ -1369,6 +1372,7 @@ function def_drag() {
                 go_grid(true);
                 go_courses(true);
             }
+            }
         });
 
     drag_popup = d3.drag()
@@ -1388,6 +1392,7 @@ function def_drag() {
                 infos.x = d.x ;
                 infos.y = d.y ;
             }
+            drag.sel = null ;
         });
 }
 
