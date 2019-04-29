48ab1b243326423517da457f95ed15ccd135c011
ricardeau <rb.web@laposte.net>
2019-04-29 14:39:55 +0200

    test if constraint method exists

--- FlOpEDT/TTapp/models.py
+++ FlOpEDT/TTapp/models.py
@@ -23,12 +23,15 @@
 # you develop activities involving the FlOpEDT/FlOpScheduler software
 # without disclosing the source code of your own applications.
 
-import importlib
 import inspect
+import importlib
 import sys
+from functools import wraps
+
+from django.conf import settings
+from django.utils.functional import lazy
 
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.conf import settings
 
 from django.db import models
 
@@ -125,64 +128,95 @@ class TTConstraint(models.Model):
     def get_viewmodel_prefetch_attributes(cls):
         return ['train_prog', 'department',]
 
+#
+#   CustomConstraint
+#
+
+
+def get_constraint_list():
+    """
+    Return constraint class list contained in CUSTOM_CONSTRAINTS_PATH
+    """
+    try:
+        module = importlib.import_module(settings.CUSTOM_CONSTRAINTS_PATH)
+        classes = inspect.getmembers(module, inspect.isclass)
+
+        constraints = []
+        for class_name, _ in classes:
+            fully_qualified_name = f'{module.__name__}.{class_name}'
+            constraints.append((fully_qualified_name, fully_qualified_name))
+
+        return constraints
+    except ModuleNotFoundError:
+        print(f"can't find the {settings.CUSTOM_CONSTRAINTS_PATH} module")
+
 
 class CustomConstraint(TTConstraint):
     """
     Call a custom constraint implementation.
-    """
-    constraint = None
-    
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-
-
-    def get_constraint_list():
-        """
-        Return constraint class list by module class inspection
-        """
-        try:
-            module = importlib.import_module(settings.CUSTOM_CONSTRAINTS_PATH)
-            classes = inspect.getmembers(module, inspect.isclass)
-
-            constraints = []
-            for class_name, _ in classes:
-                fully_qualified_name = f'{module.__name__}.{class_name}'
-                constraints.append((fully_qualified_name, fully_qualified_name))
-
-            return constraints
-        except ModuleNotFoundError:
-            print(f"can't find the {settings.CUSTOM_CONSTRAINTS_PATH} module")
-
+    """   
 
     class_name = models.CharField(
                     max_length=200,
-                    choices=get_constraint_list(),
                     null=False, 
-                    default=None, 
                     blank=False)
     groups = models.ManyToManyField('base.Group', blank=True)
     tutors = models.ManyToManyField('people.Tutor', blank=True)
-    modules = models.ManyToManyField('base.Module', blank=True)
+    modules = models.ManyToManyField('base.Module', blank=True)          
 
 
-    def get_constraint(self, fully_qualified_name):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Delay class_name field choices loading
+        self._meta.get_field('class_name').choices = lazy(get_constraint_list, list)()
+        self.constraint = None
+
+
+    def get_constraint(self, class_name):
         """
-        Return target custom class instance 
+        Return class_method located in the targeted constraint class instance
         """
         if self.constraint is None:
             try:
-                module_name, class_name = fully_qualified_name.rsplit('.', 1)
+                # Get class instance
+                module_name, class_name = class_name.rsplit('.', 1)
                 module = importlib.import_module(module_name)
                 self.constraint = getattr(module, class_name)()
             except ModuleNotFoundError:
                 print(f"can't find the <{module_name}> module")
             except:
-                print(f"an error has occured while loading class <{fully_qualified_name}>")
+                print(f"an error has occured while loading class <{class_name}>")
         
         return self.constraint
 
 
-    def enrich_model(self, ttmodel, ponderation=1):
+    def get_method(self, method_name):
+        """
+        Return the method reference by inspecting the constraint instance
+        """
+        method = None
+        constraint = self.get_constraint(self.class_name)
+        if constraint:
+            method = getattr(constraint, method_name, None)          
+        return method
+
+
+    def inject_method(func):
+        """
+        This decorator lookup for a method, in the class described by 
+        class_name attribute, with the same name as the decorated method.
+        Once retrieve the method is then injected as a method keyword parameter.
+        """
+        @wraps(func)
+        def _wrapper(self, *args, **kwargs):
+            method = self.get_method(func.__name__)
+            return func(self, *args, injected_method=method, **kwargs)
+
+        return _wrapper
+
+
+    @inject_method
+    def enrich_model(self, ttmodel, ponderation=1, injected_method=None):
         """
         Call custom constraint method
         """ 
@@ -198,27 +232,31 @@ class CustomConstraint(TTConstraint):
 
         if self.modules.count():
             args.update({'modules': list(self.modules.all())})                        
+        
+        if injected_method:
+            injected_method(ttmodel, ponderation, **args)
 
-        constraint = self.get_constraint(self.class_name)
-        if constraint:
-            constraint.enrich_model(ttmodel, ponderation, **args)
-
-
-    def one_line_description(self):    
-        constraint = self.get_constraint(self.class_name)
-        if constraint:
-            description = constraint.one_line_description()
+    
+    @inject_method
+    def one_line_description(self, injected_method=None):    
+        description = ''
+        if injected_method:
+            description = injected_method()
             if not description:                
-                description = constraint.__name__
+                description = self.class_name
         return description
 
 
-    def get_viewmodel(self):
+    @inject_method
+    def get_viewmodel(self, injected_method=None):
         view_model = super().get_viewmodel()
         details = view_model['details']
-        constraint = self.get_constraint(self.class_name)
-        if constraint:
-            details.update(constraint.get_viewmodel())
+        if injected_method:
+            details.update({'class': self.class_name})
+            details.update(injected_method())
+        else:
+            details.update({'class': f'{self.class_name} class not found'})
+
         return view_model
 
 
