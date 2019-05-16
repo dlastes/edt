a1a8ea0484b989c2477fbca9b47d8d3de7cefcc7
Pablo <pablo.seban@univ-tlse2.fr>
2019-05-16 14:17:58 +0200

    last?

--- FlOpEDT/misc/deploy_database/make_planif_file.py
+++ FlOpEDT/misc/deploy_database/make_planif_file.py
@@ -78,6 +78,7 @@ def make_planif_file(department, empty_bookname=empty_bookname):
     new_book.create_sheet('Recap')
     last_row = {}
     last_column_letter = {}
+    first_column_letter = {}
     if department.abbrev in ['INFO', 'RT', 'GIM', 'CS']:
         CT = order_CT(department)
     else:
@@ -89,19 +90,21 @@ def make_planif_file(department, empty_bookname=empty_bookname):
         ################ Writing line 1 with weeks ################
         rank = 1
         FIRST_WEEK_COL = 8
+        first_column_letter[p] = column_letter(FIRST_WEEK_COL)
         week_col = FIRST_WEEK_COL
 
         if p.starting_week < p.ending_week:
             weeks = p.ending_week - p.starting_week + 1
-            cols = weeks + 7
+            cols = weeks + 8
             append_row(sheet, empty_rows, 1, rank, cols)
             for i in range(p.starting_week, p.ending_week + 1):
                 sheet.cell(row=rank, column=week_col).value = i
                 week_col += 1
+            TOTAL_COL = week_col
 
         else:
             weeks = (53 - p.starting_week) + p.ending_week
-            cols = weeks + 7
+            cols = weeks + 8
             append_row(sheet, empty_rows, 1, rank, cols)
             for i in range(p.starting_week, 53):
                 sheet.cell(row=rank, column=week_col).value = i
@@ -110,10 +113,12 @@ def make_planif_file(department, empty_bookname=empty_bookname):
             for i in range(1, p.ending_week + 1):
                 sheet.cell(row=rank, column=week_col).value = i
                 week_col += 1
+            TOTAL_COL = week_col
+        sheet.cell(row=rank, column=week_col).value = "TOTAL"
         rank += 1
         append_row(sheet, empty_rows, 5, rank, cols)
         rank += 1
-        last_column_letter[p] = column_letter(cols)
+        last_column_letter[p] = column_letter(cols-1)
         append_row(sheet, empty_rows, 4, rank, cols)
         rank += 1
         first_line = rank
@@ -129,9 +134,13 @@ def make_planif_file(department, empty_bookname=empty_bookname):
                 sheet.cell(row=rank, column=5).value = 'Prof'
                 sheet.cell(row=rank, column=6).value = 'Type de Salle'
                 sheet.cell(row=rank, column=7).value = 'Groupes'
+                sheet.cell(row=rank, column=TOTAL_COL).value = '=SUM(%s%d:%s%d)' % (first_column_letter[p], rank,
+                                                                                    last_column_letter[p], rank)
                 rank += 1
-                if Group.objects.filter(type__in=ct.group_types.all(), train_prog=mod.train_prog).count() > 0:
-                    for g in Group.objects.filter(type__in=ct.group_types.all(), train_prog=mod.train_prog):
+                groups = Group.objects.filter(type__in=ct.group_types.all(), train_prog=mod.train_prog)
+                nb_groups = groups.count()
+                if nb_groups > 0:
+                    for g in groups:
                         append_row(sheet, empty_rows, 3, rank, cols)
                         sheet.cell(row=rank, column=1).value = mod.abbrev
                         sheet.cell(row=rank, column=2).value = '=$C%d&"_"&$E%d' % (rank, rank)
@@ -139,6 +148,13 @@ def make_planif_file(department, empty_bookname=empty_bookname):
                         sheet.cell(row=rank, column=4).value = ct.duration
                         sheet.cell(row=rank, column=7).value = g.nom
                         rank += 1
+                    sheet.cell(row=rank-nb_groups, column=TOTAL_COL).value =\
+                        '=IF(SUM(%s%d:%s%d)=$%s%d*%d,"OK","/!\\ -> "&SUM(%s%d:%s%d))' % \
+                        (first_column_letter[p], rank-nb_groups,
+                         last_column_letter[p], rank-1,
+                         column_letter(TOTAL_COL), rank-nb_groups-1,
+                         nb_groups, first_column_letter[p], rank-nb_groups,
+                         last_column_letter[p], rank-1)
                 else:
                     append_row(sheet, empty_rows, 3, rank, cols)
                     sheet.cell(row=rank, column=1).value = mod.abbrev
@@ -153,13 +169,17 @@ def make_planif_file(department, empty_bookname=empty_bookname):
 
         ############ TOTAL line ############
         ligne_finale = rank - 2
+        sheet.cell(row=rank-1, column=TOTAL_COL).value = 'TOTAL'
         append_row(sheet, empty_rows, 5, rank, cols)
-        for week_col in range(FIRST_WEEK_COL, cols+1):
+        for week_col in range(FIRST_WEEK_COL, cols):
             cl = column_letter(week_col)
             sheet.cell(row=rank, column=week_col).value = \
                 '=SUMPRODUCT((D$%d:D$%d)*(%s$%d:%s$%d)*(G$%d:G$%d="Groupes"))/60' \
                 % (first_line, ligne_finale, cl, first_line, cl, ligne_finale, first_line, ligne_finale)
             sheet.cell(row=first_line-2, column=week_col).value = '=%s%d' % (cl, rank)
+        sheet.cell(row=rank, column=TOTAL_COL).value = '=SUM(%s%d:%s%d)' % (first_column_letter[p], rank,
+                                                                            last_column_letter[p], rank)
+        sheet.cell(row=first_line-2, column=TOTAL_COL).value = '=%s%d' % (column_letter(TOTAL_COL), rank)
         rank += 1
 
         ############ Other TOTAL lines ############
@@ -172,9 +192,10 @@ def make_planif_file(department, empty_bookname=empty_bookname):
         sheet.cell(row=rank, column=2).value = "='Recap'!$B$1"
         sheet.cell(row=rank, column=6).value = '="TOTAL_"&$B$%d' % rank
         prof_row = rank
-        for week_col in range(FIRST_WEEK_COL, cols + 1):
+        for week_col in range(FIRST_WEEK_COL, cols):
             cl = column_letter(week_col)
             sheet.cell(row=rank, column=week_col).value = '=%s1' % cl
+        sheet.cell(row=rank, column=TOTAL_COL).value = 'TOTAL'
         #sheet.row_dimensions[rank].hidden = True
         rank += 1
         for ct in CT:
@@ -182,19 +203,23 @@ def make_planif_file(department, empty_bookname=empty_bookname):
             sheet.cell(row=rank, column=2).value = '=$F%d&"_"&$B$%d' % (rank, prof_row)
             sheet.cell(row=rank, column=6).value = ct.name
             sheet.cell(row=rank, column=7).value = '=SUM(H%d:%s%d)' % (rank, last_column_letter[p], rank)
-            for week_col in range(FIRST_WEEK_COL, cols + 1):
+            for week_col in range(FIRST_WEEK_COL, cols):
                 cl = column_letter(week_col)
                 sheet.cell(row=rank, column=week_col).value =\
                     '=SUMPRODUCT((D$%d:D$%d)*(%s$%d:%s$%d)*($B$%d:$B$%d=$B%d))/60' \
                     % (first_line, ligne_finale, cl, first_line, cl, ligne_finale, first_line, ligne_finale, rank)
+            sheet.cell(row=rank, column=TOTAL_COL).value = '=SUM(%s%d:%s%d)' % (first_column_letter[p], rank,
+                                                                                last_column_letter[p], rank)
             #sheet.row_dimensions[rank].hidden = True
             rank += 1
         append_row(sheet, empty_rows, 8, rank, cols)
         nb_ct = len(CT) #CourseType.objects.filter(department=department).count()
-        for week_col in range(FIRST_WEEK_COL-1, cols + 1):
+        for week_col in range(FIRST_WEEK_COL-1, cols):
             cl = column_letter(week_col)
             sheet.cell(row=rank, column=week_col).value = \
                 '=SUM(%s%d:%s%d)' % (cl, rank - nb_ct, cl, rank - 1)
+        sheet.cell(row=rank, column=TOTAL_COL).value = '=SUM(%s%d:%s%d)' % (first_column_letter[p], rank,
+                                                                            last_column_letter[p], rank)
         last_row[p.name] = rank
         rank += 1
 
