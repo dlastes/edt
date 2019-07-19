b7918fdc0a2d1e86f8436366772c39a874c32f18
PRG <prenaud@irit.fr>
2019-07-19 20:26:08 +0200

    week, work_copy changes

--- FlOpEDT/TTapp/static/TTapp/slide_panel.js
+++ FlOpEDT/TTapp/static/TTapp/slide_panel.js
@@ -1,49 +1,68 @@
+// open side pannel
 function openNav() {
+
+    is_side_pannel_open = true ;
+
+    // open pannel
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("edt-main").style.marginLeft = "250px";
     document.getElementById("menu-edt").style.marginLeft = "250px";
 
+    // add event listener
+    $('#dd_work_copy').on('change', function (e) {
+        num_copie = this.value;
+        fetch_all(false, false);
+    });
+
+    // fetch the copy numbers
+    fetch_work_copy_numbers();
+}
+
+// Fetch the available work copy numbers for the current week
+function fetch_work_copy_numbers() {
     var cur_week = weeks.init_data[weeks.sel[0]].semaine;
     var cur_year = weeks.init_data[weeks.sel[0]].an;
 
     $.ajax({
         type: "GET",
-        dataType: 'text',
-        url: url_side_pannel +  cur_year + '/' + cur_week,
+        dataType: 'json',
+        url: url_work_copies +  cur_year + '/' + cur_week,
         async: true,
-        contentType: "text/html",
-        success: function(msg, ts, req) {
-            console.log(msg);
-            document.getElementById('mySidebar').innerHTML = msg ;
+        contentType: "application/json; charset=utf-8",
+        success: function(msg) {
+            update_work_copy_numbers(msg.copies) ;
         },
         error: function(msg) {
             console.log("error");
         }
     });
+
+}
+
+// refresh the work copy dropdown list
+function update_work_copy_numbers(copies) {
+    // remove old copy numbers
+    var dd = document.getElementById("dd_work_copy");
+    while (dd.firstChild) {
+        dd.removeChild(dd.firstChild);
+    }
     
+    // add new copy numbers
+    var option ;
+    for (var i=0 ; i<copies.length ; i++){
+        option = {value:copies[i], text:copies[i]} ;
+        $('#dd_work_copy').append($('<option>', option));
+    }
 }
 
+// close the side pannel
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("edt-main").style.marginLeft= "0";
-  document.getElementById("menu-edt").style.marginLeft= "0";
-}
+    // avoid fetching work copy numbers if closed
+    is_side_pannel_open = false ;
 
-function getForm( funcname ) {
-	
-	$.ajax({
-        type: "GET",
-        dataType: 'text',
-        url: "ttapp/viewForm/" + funcname,
-        async: true,
-        contentType: "text/html",
-        success: function(msg, ts, req) {
-            console.log( msg );
-            document.getElementById('sideBarForm').innerHTML = msg;
-        },
-        error: function(msg) {
-            console.log("error");
-        }
-    });
+    // close pannel
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("edt-main").style.marginLeft= "0";
+    document.getElementById("menu-edt").style.marginLeft= "0";
 
 }
