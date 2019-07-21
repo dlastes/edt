838d823edcc30b9a41dac2ac53b66a31294644db
PRG <prenaud@irit.fr>
2019-07-21 13:42:52 +0200

    fill dropdown lists

--- FlOpEDT/base/static/base/stype_side_panel.js
+++ FlOpEDT/base/static/base/stype_side_panel.js
@@ -1,5 +1,35 @@
 var tutor_list = [] ;
 
+var dds = {tutor:{url: url_all_tutors, id:"dd_tutors"},
+           type:{url: url_course_types, id:"dd_course_types"},
+           prog:{url: url_training_programmes, id:"dd_programmes"}};
+
+
+// fetch tutors whose main department is the current department,
+// and fill the dropdown list
+function fetch_infos_dd(dd){
+    $.ajax({
+        type: "GET",
+        dataType: 'json',
+        url: dd.url,
+        async: true,
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            // add tutors in the dropdown list
+            data.sort() ;
+            var option ;
+            for (var i=0 ; i<data.length ; i++){
+                option = {value:data[i], text:data[i]} ;
+                $('#'+dd.id).append($('<option>', option));
+            }
+        },
+        error: function(msg) {
+            console.log("error");
+        }
+    });
+};
+
+
 // open side panel
 function openNav() {
 
@@ -13,14 +43,16 @@ function openNav() {
     $('#sp_tutor').hide();
     $('#sp_course_type').hide();
 
-    // add event listener
-    $('#dd_tutors').on('change', function (e) {
+    // add event listeners
+    $('#'+dds.tutor.id).on('change', function (e) {
         user.nom = this.value;
         fetch_pref_only();
     });
 
-    // fetch the copy numbers
-    fetch_tutor_list();
+    // fetch the tutors, course types and training programmes
+    fetch_infos_dd(dds.tutor);
+    fetch_infos_dd(dds.type);
+    fetch_infos_dd(dds.prog);
 }
 
 
@@ -35,31 +67,6 @@ function closeNav() {
     document.getElementById("menu-edt").style.marginLeft= "0";
 }
 
-// fetch tutors whose main department is the current department,
-// and fill the dropdown list
-function fetch_tutor_list(){
-    $.ajax({
-        type: "GET",
-        dataType: 'json',
-        url: url_all_tutors,
-        async: true,
-        contentType: "application/json; charset=utf-8",
-        success: function(msg) {
-            console.log(msg);
-            // add tutors
-            var tutors = msg.tutors ;
-            tutors.sort() ;
-            var option ;
-            for (var i=0 ; i<tutors.length ; i++){
-                option = {value:tutors[i], text:tutors[i]} ;
-                $('#dd_tutors').append($('<option>', option));
-            }
-        },
-        error: function(msg) {
-            console.log("error");
-        }
-    });
-};
 
 // show tutor selection in the side panel
 function show_tutors() {
