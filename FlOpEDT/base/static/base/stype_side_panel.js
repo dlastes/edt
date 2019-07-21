ee8c3db2be0c2210f70dccecdd5968b6403974b3
PRG <prenaud@irit.fr>
2019-07-21 20:38:35 +0200

    dd management in js

--- FlOpEDT/base/static/base/stype_side_panel.js
+++ FlOpEDT/base/static/base/stype_side_panel.js
@@ -1,34 +1,77 @@
-var tutor_list = [] ;
+// drop down list parameters
+dd_selections['tutor'].url = url_all_tutors ;
+dd_selections['tutor'].id ="#dd_tutors";
+dd_selections['type'].url = url_course_types ;
+dd_selections['type'].id ="#dd_course_types";
+dd_selections['prog'].url = url_training_programmes;
+dd_selections['prog'].id = "#dd_programmes" ;
 
-var dds = {tutor:{url: url_all_tutors, id:"dd_tutors"},
-           type:{url: url_course_types, id:"dd_course_types"},
-           prog:{url: url_training_programmes, id:"dd_programmes"}};
+for (var key in dd_selections){
+    dd_selections[key].filled = false ;
+}
 
 
-// fetch tutors whose main department is the current department,
-// and fill the dropdown list
+// fetch data and fill the dropdown list
 function fetch_infos_dd(dd){
-    $.ajax({
-        type: "GET",
-        dataType: 'json',
-        url: dd.url,
-        async: true,
-        contentType: "application/json; charset=utf-8",
-        success: function(data) {
-            // add tutors in the dropdown list
-            data.sort() ;
-            var option ;
-            for (var i=0 ; i<data.length ; i++){
-                option = {value:data[i], text:data[i]} ;
-                $('#'+dd.id).append($('<option>', option));
+    if (!dd.filled) {
+        console.log(dd.url);        
+        $.ajax({
+            type: "GET",
+            dataType: 'json',
+            url: dd.url,
+            async: true,
+            contentType: "application/json; charset=utf-8",
+            success: function(data) {
+                // add elements in the dropdown list
+                data.sort() ;
+                var option ;
+                for (var i=0 ; i<data.length ; i++){
+                    option = {value:data[i], text:data[i]} ;
+                    $(dd.id).append($('<option>', option));
+                }
+                dd.filled = true ;
+                dd_fetch_ended() ;
+            },
+            error: function(msg) {
+                console.log("error");
             }
-        },
-        error: function(msg) {
-            console.log("error");
-        }
-    });
+        });
+    }
 };
 
+
+
+// read selected values in dd lists and fetch corresponding data
+function fetch_selected() {
+    for(var key in dd_selections) {
+        dd_selections[key].value = $(dd_selections[key].id).find(':selected').val() ;
+    }
+    if (mode == 'tutor') {
+        user.nom = dd_selections['tutor'].value;
+    } else {
+        user.nom = course_type_prog_name(
+            dd_selections['prog'].value,
+            dd_selections['type'].value) ;
+    }
+    fetch_pref_only();
+}
+
+
+// save the default values of the dd lists when filled
+function dd_fetch_ended() {
+    if (mode == 'tutor' && dd_selections['tutor'].filled) {
+        // select logged user if present
+        var lolo = 'CD' ;
+        // logged_usr.nom
+        var me = $(dd_selections['tutor'].id + " option[value='" + lolo + "']");
+        if (typeof me !== 'undefined') {
+            me.prop('selected', true);
+        }
+        fetch_selected();
+    } else if (mode == 'course' && dd_selections['type'].filled && dd_selections['prog'].filled) {
+        fetch_selected();
+    }
+}
 
 // open side panel
 function openNav() {
@@ -40,19 +83,21 @@ function openNav() {
     document.getElementById("content").style.marginLeft = "250px";
     document.getElementById("menu-edt").style.marginLeft = "250px";
 
-    $('#sp_tutor').hide();
+//    $('#sp_tutor').hide();
     $('#sp_course_type').hide();
 
     // add event listeners
-    $('#'+dds.tutor.id).on('change', function (e) {
-        user.nom = this.value;
-        fetch_pref_only();
-    });
+    for (var key in dd_selections) {
+        $(dd_selections[key].id).on('change', function (e) {
+            fetch_selected();
+        });
+    }
+    
 
     // fetch the tutors, course types and training programmes
-    fetch_infos_dd(dds.tutor);
-    fetch_infos_dd(dds.type);
-    fetch_infos_dd(dds.prog);
+    for (var key in dd_selections) {
+        fetch_infos_dd(dd_selections[key]);
+    }
 }
 
 
@@ -70,14 +115,25 @@ function closeNav() {
 
 // show tutor selection in the side panel
 function show_tutors() {
+    // refresh side panel
     $('#sp_course_type').hide();
     $('#sp_tutor').show();
 
-    
+    // change button behaviors
+    mode = "tutor" ;
+
+    fetch_selected();
 }
+
+
 
 // show course type selection in the side panel
 function show_course_types(){
     $('#sp_tutor').hide();
     $('#sp_course_type').show();
+
+    // change button behaviors
+    mode = "course" ;
+
+    fetch_selected();
 }
