a5b13559549874f5f2bcccaab142bb9a05e8bb44
Pablo <pablo.seban@univ-tlse2.fr>
2019-11-16 00:13:39 +0100

    other department...

--- FlOpEDT/TTapp/TTModel.py
+++ FlOpEDT/TTapp/TTModel.py
@@ -1118,13 +1118,22 @@ class TTModel(object):
                         occupied_in_another_department = True
                 if occupied_in_another_department:
                     name = 'other_dep_' + str(i) + '_' + str(sl) + '_' + str(self.constraint_nb)
+                    # self.add_constraint(self.sum(self.TT[(sl, c)]
+                    #                              for c in (self.wdb.courses_for_tutor[i]
+                    #                                        | self.wdb.courses_for_supp_tutor[i])),
+                    #                     '==',
+                    #                     0,
+                    #                     name=name)
+                    # self.add_constraint(self.IBS[(i, sl)], '==', 1)
+                    # self.add_constraint(self.IBD[(i, sl.day)], '==', 1)
                     self.add_constraint(self.sum(self.TT[(sl, c)]
                                                  for c in (self.wdb.courses_for_tutor[i]
-                                                           | self.wdb.courses_for_supp_tutor[i])),
+                                                           | self.wdb.courses_for_supp_tutor[i]))
+                                        - self.IBS[i, sl]
+                                        - self.IBD[i, sl.day],
                                         '==',
-                                        0,
+                                        -2,
                                         name=name)
-
 
     def add_specific_constraints(self):
         """
