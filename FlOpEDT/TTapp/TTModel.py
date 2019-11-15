8a11af99c44c30e68bc84f9cf4c02dae750ca886
Pablo <pablo.seban@univ-tlse2.fr>
2019-11-16 00:15:23 +0100

    other department debug

--- FlOpEDT/TTapp/TTModel.py
+++ FlOpEDT/TTapp/TTModel.py
@@ -1127,8 +1127,10 @@ class TTModel(object):
                     # self.add_constraint(self.IBS[(i, sl)], '==', 1)
                     # self.add_constraint(self.IBD[(i, sl.day)], '==', 1)
                     self.add_constraint(self.sum(self.TT[(sl, c)]
-                                                 for c in (self.wdb.courses_for_tutor[i]
-                                                           | self.wdb.courses_for_supp_tutor[i]))
+                                                 for c in ((self.wdb.courses_for_tutor[i]
+                                                            | self.wdb.courses_for_supp_tutor[i])
+                                                           & self.wdb.compatible_courses[sl])
+                                                 )
                                         - self.IBS[i, sl]
                                         - self.IBD[i, sl.day],
                                         '==',
