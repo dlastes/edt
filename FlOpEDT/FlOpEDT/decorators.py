57e7a2bcc6c12c1b15798432ea2c55c9872693a5
PRG <prenaud@irit.fr>
2020-02-07 16:51:36 +0100

    forgot import

--- FlOpEDT/FlOpEDT/decorators.py
+++ FlOpEDT/FlOpEDT/decorators.py
@@ -28,6 +28,7 @@ from functools import wraps
 from urllib.parse import urlparse
 
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import resolve_url
 
 def request_passes_test(test_func, login_url=None, redirect_field_name=REDIRECT_FIELD_NAME):
