5fe91426df8f4ab32116930092904f6b3368252b
tgouvei <thomas.gouveia@etu.univ-tlse2.fr>
2020-02-27 10:51:54 +0100

    Correction merge des branches

--- FlOpEDT/base/models.py
+++ FlOpEDT/base/models.py
@@ -307,10 +307,11 @@ class ModulePossibleTutors(models.Model):
 
 class ModuleDescription(models.Model):
     module = models.OneToOneField('Module', on_delete=models.CASCADE)
-    desc = models.TextField()
+    desc = models.TextField(
+        default="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 
-
-
+    def __str__(self):
+        return '{} : {}'.format(self.module, self.desc)
 
 
 class CourseType(models.Model):
