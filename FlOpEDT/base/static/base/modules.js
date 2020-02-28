93a8b69c74d0fb8d66ebec47ee28b7907f08ac50
tgouvei <thomas.gouveia@etu.univ-tlse2.fr>
2020-02-28 16:23:04 +0100

    Fix javacript and modal

--- FlOpEDT/base/static/base/modules.js
+++ FlOpEDT/base/static/base/modules.js
@@ -27,14 +27,14 @@ class ModulesApp {
     constructor(modules, container, search, filters) {
         this.modules = modules;
         this.promos = ['ALL'];
-        this.modules.forEach(e => !this.promos.includes(e.promo) ? this.promos.push(e.promo) : null);
-        this.promos.sort();
         this.filters = filters;
         this.selectedFilter = this.promos[0];
         this.searchResult = modules;
         this.container = container;
         this.search = search;
-        this.search.addEventListener('input', this.searchModule);
+        this.search.addEventListener('input', e => this.searchModule(e));
+        this.modules.forEach(e => !this.promos.includes(e.promo) ? this.promos.push(e.promo) : null);
+        this.promos.sort();
         this.render();
     }
 
@@ -53,15 +53,28 @@ class ModulesApp {
         let card = document.createElement('div');
         card.setAttribute("class", "module__card");
         card.innerHTML = `
+            <div id="modal${mod.abbrev}" class="module__modal">
+                  <div class="module__modal__background"></div>
+                  <div class="module__modal__content">
+                        <h1>Decription du module</h1>
+                      <p>
+                      ${mod.description}
+                      </p>
+                      <span><strong>Responsable du module :</strong> ${mod.resp_last_name} ${mod.resp_first_name}</span>
+                  </div>
+              <button class="module__modal__close" aria-label="close"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg></button>
+            </div>
             <div class="left_vertical" style="background: ${mod.color_bg}; color: ${mod.color_txt}">
                 <h3 class="module__abbrev">${mod.abbrev}</h3>
             </div>
             <div class="right">
                 <span class="module__name">${mod.name}</span>
                 <span class="module__resp">${mod.resp}</span>
-                <span class="module__promo">${mod.promo}</span>
             </div>
         `;
+        card.addEventListener('click', e => {
+            document.getElementById(`modal${mod.abbrev}`).classList.toggle('is-open');
+        });
         this.container.append(card);
     }
 
@@ -79,7 +92,7 @@ class ModulesApp {
         let promo = selected.getAttribute('id');
         $(`#${promo}`).siblings().removeClass('filter__active');
         selected.classList.add('filter__active');
-        this.searchResult = promo == 'ALL' ? this.modules : this.modules.filter(mod => mod.promo == promo);
+        this.searchResult = promo === 'ALL' ? this.modules : this.modules.filter(mod => mod.promo === promo);
         this.selectedFilter = promo;
         this.clearModules();
         this.render();
@@ -88,6 +101,7 @@ class ModulesApp {
 
     searchModule(e) {
         let searchQuery = e.target.value;
+        this.selectedFilter = 'ALL';
         this.searchResult = this.modules.filter(mod => mod.abbrev.toLowerCase().startsWith(searchQuery.toLowerCase()) ||
             mod.name.toLowerCase().startsWith(searchQuery.toLowerCase()));
         this.clearModules();
