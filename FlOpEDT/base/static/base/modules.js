8a0e7772a8f63cb2b79178076bbe890e6a41b109
Etudiant <hugoamalric@gmail.com>
2020-02-28 09:01:56 +0100

    Recherche des modules

--- FlOpEDT/base/static/base/modules.js
+++ FlOpEDT/base/static/base/modules.js
@@ -19,27 +19,48 @@ class ModulesApp{
 
     static init(modules){
         let container = document.getElementById("modules-container");
-        let app = new ModulesApp(modules, container);
+        let search = document.getElementById("modules-search");
+        let app = new ModulesApp(modules, container, search);
         app.render();
     }
 
-    constructor(modules, container){
+    constructor(modules, container, search){
         this.modules = modules;
+        this.searchResult = modules;
         this.container = container;
+        this.search = search;
+        this.search.addEventListener('input', this.searchModule);
     }
 
     render = () => {
-        this.modules.forEach(this.buildModules);
+        this.searchResult.forEach(this.buildModules);
     }
 
     buildModules = mod => {
         let card = document.createElement('div');
+        card.setAttribute("class", "module__card");
         card.innerHTML = `
             <h3 class="module__abbrev">${mod.abbrev}</h3>
             <span class="module__name">${mod.name}</span>
             <span class="module__resp">${mod.resp}</span>
         `;
         this.container.append(card);
+    }
+
+    searchModule = e => {
+        let searchQuery = e.target.value;
+        this.searchResult = this.modules.filter(mod => mod.abbrev.toLowerCase().startsWith(searchQuery.toLowerCase()) || 
+                                                       mod.name.toLowerCase().startsWith(searchQuery.toLowerCase())) ;
+        this.clearModules();
+        this.render();
+    }
+
+    clearModules = () => {
+        let child = this.container.lastElementChild;
+        while(child){
+            this.container.removeChild(child);
+            child = this.container.lastElementChild;
+        }
     }
 }
 
