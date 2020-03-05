dee4f613f1de80e17fb31d899db10f331ed91562
michael <michael.zerbib31@gmail.com>
2020-03-05 16:32:56 +0100

    camel2snake

--- FlOpEDT/TTapp/constraint.py
+++ FlOpEDT/TTapp/constraint.py
@@ -1,16 +1,17 @@
-def singular_or_plurial(myList, isMale):
-    determinant = "les" if len(myList) > 1 else ("le" if isMale else "la")
-    conjugation = "s" if len(myList) > 1 else ""
+def singular_or_plurial(my_list, is_male):
+    determinant = "les" if len(my_list) > 1 else ("le" if is_male else "la")
+    conjugation = "s" if len(my_list) > 1 else ""
     return determinant, conjugation
 
-def list2str(myList):
-    if len(myList) == 1:
-        return myList[0]
 
+def list2str(my_list):
+    if len(my_list) == 1:
+        return my_list[0]
     output = "["
-    for i in range(len(myList) - 1):
-        output += myList[i] + ", "
-    return output + myList[-1] + "]"
+    for i in range(len(my_list) - 1):
+        output += my_list[i] + ", "
+    return output + my_list[-1] + "]"
+
 
 class Constraint:
     def __init__(self, id, constraint_type=None, instructors=[], slots=[], courses=[], weeks=[], rooms=[],
@@ -56,40 +57,40 @@ class Constraint:
             res += 'de type "%s" ; ' % str(self.constraint_type)
 
         if len(self.instructors) >= 1:
-            d, c = singular_or_plurial(self.instructors, isMale=True)
+            d, c = singular_or_plurial(self.instructors, is_male=True)
             res += "pour %s professeur%s %s ; " % (d, c, list2str([str(instructor) for instructor in self.instructors]))
 
         if len(self.courses) >= 1:
-            d, _ = singular_or_plurial(self.courses, isMale=True)
+            d, _ = singular_or_plurial(self.courses, is_male=True)
             res += "pour %s cours %s ; " % (d, list2str([str(course) for course in self.courses]))
 
         if len(self.slots) >= 1:
-            d, c = singular_or_plurial(self.slots, isMale=True)
+            d, c = singular_or_plurial(self.slots, is_male=True)
             res += "pour %s slot%s %s ; " % (d, c, list2str([str(slot) for slot in self.slots]))
 
         if len(self.rooms) >= 1:
-            d, c = singular_or_plurial(self.rooms, isMale=False)
+            d, c = singular_or_plurial(self.rooms, is_male=False)
             res += "pour %s salle%s %s ; " % (d, c, list2str([str(room) for room in self.rooms]))
 
         if len(self.weeks) >= 1:
-            d, c = singular_or_plurial(self.weeks, isMale=False)
+            d, c = singular_or_plurial(self.weeks, is_male=False)
             res += "pour %s semaine%s %s ; " % (d, c, list2str([str(week) for week in self.weeks]))
 
         if len(self.groups) >= 1:
-            d, c = singular_or_plurial(self.groups, isMale=True)
+            d, c = singular_or_plurial(self.groups, is_male=True)
             res += "pour %s groupe%s %s ; " % (d, c, list2str([str(group) for group in self.groups]))
 
         if len(self.days) >= 1:
-            d, c = singular_or_plurial(self.days, isMale=True)
+            d, c = singular_or_plurial(self.days, is_male=True)
             res += "pour %s jour%s %s ; " % (d, c, list2str([str(day) for day in self.days]))
 
         if len(self.departments) >= 1:
-            d, c = singular_or_plurial(self.departments, isMale=True)
+            d, c = singular_or_plurial(self.departments, is_male=True)
             res += "pour %s departement%s %s ; " % (d, c, list2str(
                 [str(department) for department in self.departments]))
 
         if len(self.modules) >= 1:
-            d, c = singular_or_plurial(self.modules, isMale=True)
+            d, c = singular_or_plurial(self.modules, is_male=True)
             res += "pour %s module%s %s ; " % (d, c, list2str([str(module) for module in self.modules]))
 
         if self.apm is not None:
