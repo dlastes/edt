05620b05307a79aaefa2b4bdc1d88e5c006e5850
KevinDelcourt <delcokev@gmail.com>
2020-03-16 14:05:36 +0200

    Templates for everything but tutors/rooms/param

--- FlOpEDT/flopeditor/views.py
+++ FlOpEDT/flopeditor/views.py
@@ -34,16 +34,15 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import user_passes_test
 from base.models import Department, TimeGeneralSettings, Day
-from FlOpEDT.decorators import dept_admin_required
+from FlOpEDT.decorators import dept_admin_required, tutor_required
 from base.timing import min_to_str, str_to_min
 from base.check_admin import check_admin
 from people.models import Tutor
-from flopeditor.check_tutor import check_tutor
 from flopeditor.db_requests import create_departments_in_database
 from flopeditor.validator import validate_department_creation, validate_parameters_edit, OK_RESPONSE
 
 
-@user_passes_test(check_tutor)
+@tutor_required
 def home(request):
     """Main view of FlopEditor.
 
@@ -62,7 +61,7 @@ def home(request):
     })
 
 
-@user_passes_test(check_tutor)
+@tutor_required
 def department_default(request, department_abbrev):
     """Redirects to default department view.
 
@@ -78,7 +77,7 @@ def department_default(request, department_abbrev):
                     department_abbrev=department_abbrev)
 
 
-@user_passes_test(check_tutor)
+@tutor_required
 def department_parameters(request, department_abbrev):
     """Parameters view of FlopEditor.
 
@@ -109,7 +108,7 @@ def department_parameters(request, department_abbrev):
 
     })
 
-@user_passes_test(check_tutor)
+@tutor_required
 def department_parameters_edit(request, department_abbrev):
     """Parameters edit view of FlopEditor.
 
@@ -137,132 +136,6 @@ def department_parameters_edit(request, department_abbrev):
         'default_preference_duration': min_to_str(parameters.default_preference_duration),
         'has_department_perm': request.user.has_department_perm(department=department, admin=True),
         'edit': True
-    })
-
-@user_passes_test(check_tutor)
-def department_rooms(request, department_abbrev):
-    """Rooms view of FlopEditor.
-
-    :param request:           Client request.
-    :param department_abbrev: Department abbreviation.
-    :type request:            django.http.HttpRequest
-    :type department_abbrev:  str
-    :return: Parameters page rendered from the rooms template of FlopEditor.
-    :rtype:  django.http.HttpResponse
-
-    """
-    department = get_object_or_404(Department, abbrev=department_abbrev)
-    departments = Department.objects.exclude(abbrev=department_abbrev)
-    return render(request, "flopeditor/rooms.html", {
-        'title': 'Salles',
-        'department': department,
-        'list_departments': departments
-    })
-
-
-@user_passes_test(check_tutor)
-def department_student_groups(request, department_abbrev):
-    """Groups view of FlopEditor.
-
-    :param request:           Client request.
-    :param department_abbrev: Department abbreviation.
-    :type request:            django.http.HttpRequest
-    :type department_abbrev:  str
-    :return: Parameters page rendered from the groups template of FlopEditor.
-    :rtype:  django.http.HttpResponse
-
-    """
-    department = get_object_or_404(Department, abbrev=department_abbrev)
-    departments = Department.objects.exclude(abbrev=department_abbrev)
-    return render(request, "flopeditor/student_groups.html", {
-        'title': 'Groupes d\'élèves',
-        'department': department,
-        'list_departments': departments,
-        'has_dept_perm': request.user.has_department_perm(department=department, admin=True),
-    })
-
-@user_passes_test(check_tutor)
-def department_training_programmes(request, department_abbrev):
-    """Groups view of FlopEditor.
-
-    :param request:           Client request.
-    :param department_abbrev: Department abbreviation.
-    :type request:            django.http.HttpRequest
-    :type department_abbrev:  str
-    :return: Parameters page rendered from the groups template of FlopEditor.
-    :rtype:  django.http.HttpResponse
-
-    """
-    department = get_object_or_404(Department, abbrev=department_abbrev)
-    departments = Department.objects.exclude(abbrev=department_abbrev)
-    return render(request, "flopeditor/training_programmes.html", {
-        'title': 'Promos',
-        'department': department,
-        'list_departments': departments,
-        'has_dept_perm': request.user.has_department_perm(department=department, admin=True),
-    })
-
-@user_passes_test(check_tutor)
-def department_student_group_types(request, department_abbrev):
-    """Groups view of FlopEditor.
-
-    :param request:           Client request.
-    :param department_abbrev: Department abbreviation.
-    :type request:            django.http.HttpRequest
-    :type department_abbrev:  str
-    :return: Parameters page rendered from the groups template of FlopEditor.
-    :rtype:  django.http.HttpResponse
-
-    """
-    department = get_object_or_404(Department, abbrev=department_abbrev)
-    departments = Department.objects.exclude(abbrev=department_abbrev)
-    return render(request, "flopeditor/student_group_types.html", {
-        'title': 'Natures de groupes d\'élèves',
-        'department': department,
-        'list_departments': departments,
-        'has_dept_perm': request.user.has_department_perm(department=department, admin=True),
-    })
-
-
-@user_passes_test(check_tutor)
-def department_modules(request, department_abbrev):
-    """Modules view of FlopEditor.
-
-    :param request:           Client request.
-    :param department_abbrev: Department abbreviation.
-    :type request:            django.http.HttpRequest
-    :type department_abbrev:  str
-    :return: Parameters page rendered from the modules template of FlopEditor.
-    :rtype:  django.http.HttpResponse
-
-    """
-    department = get_object_or_404(Department, abbrev=department_abbrev)
-    departments = Department.objects.exclude(abbrev=department_abbrev)
-    return render(request, "flopeditor/modules.html", {
-        'title': 'Modules',
-        'department': department,
-        'list_departments': departments
-    })
-
-
-@user_passes_test(check_tutor)
-def department_classes(request, department_abbrev):
-    """Classes view of FlopEditor.
-
-    :param request:           Client request.
-    :param department_abbrev: Department abbreviation.
-    :type request:            django.http.HttpRequest
-    :type department_abbrev:  str
-    :return: Parameters page rendered from the classes template of FlopEditor.
-    :rtype:  django.http.HttpResponse
-
-    """
-    department = get_object_or_404(Department, abbrev=department_abbrev)
-    departments = Department.objects.exclude(abbrev=department_abbrev)
-    return render(request, "flopeditor/classes.html", {
-        'title': 'Classes',
-        'department': department,
-        'list_departments': departments
     })
 
 
@@ -325,3 +198,131 @@ def ajax_edit_parameters(request, department_abbrev):
             response['message'] = "Les modifications ont bien été enregistrées."
         return JsonResponse(response)
     return HttpResponseForbidden()
+
+# Crud views
+# --------------------------------
+
+def crud_view(request,department_abbrev,view_name,title):
+    """default view rendering for paje using crudjs.
+
+    :param request:           Client request.
+    :param department_abbrev: Department abbreviation.
+    :type request:            django.http.HttpRequest
+    :type department_abbrev:  str
+    :param view_name:           Client request.
+    :type view_name:            str
+    :param title: Department abbreviation.
+    :type title:                str
+    :param department_abbrev: Department abbreviation.
+    :type department_abbrev:  str
+    :return: page rendered from the corresponding template of FlopEditor.
+    :rtype:  django.http.HttpResponse
+
+    """
+    department = get_object_or_404(Department, abbrev=department_abbrev)
+    departments = Department.objects.exclude(abbrev=department_abbrev)
+    return render(request, view_name, {
+        'title': title,
+        'department': department,
+        'list_departments': departments,
+        'has_dept_perm': request.user.has_department_perm(department=department, admin=True),
+    })
+
+@tutor_required
+def department_student_groups(request, department_abbrev):
+    """Student groups view of FlopEditor.
+
+    :param request:           Client request.
+    :param department_abbrev: Department abbreviation.
+    :type request:            django.http.HttpRequest
+    :type department_abbrev:  str
+    :return: Page rendered from the template of FlopEditor.
+    :rtype:  django.http.HttpResponse
+
+    """
+    return crud_view(request,department_abbrev,"flopeditor/student_groups.html","Groupes d'élèves")
+
+
+@tutor_required
+def department_student_group_types(request, department_abbrev):
+    """Student group types view of FlopEditor.
+
+    :param request:           Client request.
+    :param department_abbrev: Department abbreviation.
+    :type request:            django.http.HttpRequest
+    :type department_abbrev:  str
+    :return: Page rendered from the template of FlopEditor.
+    :rtype:  django.http.HttpResponse
+
+    """
+    return crud_view(request,department_abbrev,"flopeditor/student_group_types.html","Natures de groupes d'élèves")
+
+@tutor_required
+def department_course_types(request, department_abbrev):
+    """course_types view of FlopEditor.
+
+    :param request:           Client request.
+    :param department_abbrev: Department abbreviation.
+    :type request:            django.http.HttpRequest
+    :type department_abbrev:  str
+    :return: Page rendered from the template of FlopEditor.
+    :rtype:  django.http.HttpResponse
+
+    """
+    return crud_view(request,department_abbrev,"flopeditor/course_types.html","Types de cours")
+
+@tutor_required
+def department_modules(request, department_abbrev):
+    """Modules view of FlopEditor.
+
+    :param request:           Client request.
+    :param department_abbrev: Department abbreviation.
+    :type request:            django.http.HttpRequest
+    :type department_abbrev:  str
+    :return: Page rendered from the modules template of FlopEditor.
+    :rtype:  django.http.HttpResponse
+
+    """
+    return crud_view(request,department_abbrev,"flopeditor/modules.html",'Modules')
+
+@tutor_required
+def department_periods(request, department_abbrev):
+    """Periods/Semesters view of FlopEditor.
+
+    :param request:           Client request.
+    :param department_abbrev: Department abbreviation.
+    :type request:            django.http.HttpRequest
+    :type department_abbrev:  str
+    :return: Page rendered from the modules template of FlopEditor.
+    :rtype:  django.http.HttpResponse
+
+    """
+    return crud_view(request,department_abbrev,"flopeditor/periods.html",'Périodes')
+
+@tutor_required
+def department_training_programmes(request, department_abbrev):
+    """Training programme view of FlopEditor.
+
+    :param request:           Client request.
+    :param department_abbrev: Department abbreviation.
+    :type request:            django.http.HttpRequest
+    :type department_abbrev:  str
+    :return: page rendered from the training programme template of FlopEditor.
+    :rtype:  django.http.HttpResponse
+
+    """
+    return crud_view(request,department_abbrev,'flopeditor/training_programmes.html','Promos')
+
+# @tutor_required
+# def department_rooms(request, department_abbrev):
+#     """Rooms view of FlopEditor.
+
+#     :param request:           Client request.
+#     :param department_abbrev: Department abbreviation.
+#     :type request:            django.http.HttpRequest
+#     :type department_abbrev:  str
+#     :return: page rendered from the rooms template of FlopEditor.
+#     :rtype:  django.http.HttpResponse
+
+#     """
+#     return crud_view(request,department_abbrev,"flopeditor/rooms.html","Salles")
