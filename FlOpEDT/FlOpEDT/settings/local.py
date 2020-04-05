8f7454e88eb598e6dd6d6efb426aa7ae644d0ce5
Pablo <pablo.seban@univ-tlse2.fr>
2020-04-05 15:36:58 +0200

    local...

--- FlOpEDT/FlOpEDT/settings/local.py
+++ FlOpEDT/FlOpEDT/settings/local.py
@@ -30,9 +30,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'flopdb',
-        'USER': 'flopuser',
-        'PASSWORD': 'pwddb',
+        'NAME': 'flop_database_dev',
+        'USER': 'flop_user',
+        'PASSWORD': 'your_password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
