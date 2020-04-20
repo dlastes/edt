d8da2e9a91d9a84d1a89c12f31a9d755fe5b57b0
Pablo <pablo.seban@univ-tlse2.fr>
2020-04-20 22:08:40 +0200

    in MyFlOp

--- FlOpEDT/MyFlOp/models.py
+++ FlOpEDT/MyFlOp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+
+
+class ModuleTutorRepartition(models.Model):
+    module = models.ForeignKey('Module', on_delete=models.CASCADE)
+    course_type = models.ForeignKey('CourseType', on_delete=models.CASCADE)
+    tutor = models.ForeignKey('people.Tutor', on_delete=models.CASCADE)
+    week = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(53)],
+        null=True,
+        blank=True)
+    year = models.PositiveSmallIntegerField(null=True,
+                                            blank=True)
+    courses_nb = models.PositiveSmallIntegerField()
