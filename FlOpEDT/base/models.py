eab0401acfe6f59970432806d04db64adaac42a1
Pablo <pablo.seban@univ-tlse2.fr>
2020-04-20 19:39:19 +0200

    Test ModuleTutorRepartition

--- FlOpEDT/base/models.py
+++ FlOpEDT/base/models.py
@@ -399,6 +399,20 @@ class CoursePossibleTutors(models.Model):
         'people.Tutor', blank=True, related_name='shared_possible_courses')
 
 
+class ModuleTutorRepartition(models.Model):
+    module = models.ForeignKey('Module', on_delete=models.CASCADE)
+    course_type = models.ForeignKey('CourseType', on_delete=models.CASCADE)
+    tutor = models.ForeignKey('people.Tutor', on_delete=models.CASCADE)
+    week = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(53)],
+        null=True,
+        blank=True)
+    year = models.PositiveSmallIntegerField(null=True,
+                                            blank=True)
+    courses_nb = models.PositiveSmallIntegerField()
+
+
+
 class ScheduledCourse(models.Model):
     course = models.ForeignKey('Course', on_delete=models.CASCADE)
     day = models.CharField(
