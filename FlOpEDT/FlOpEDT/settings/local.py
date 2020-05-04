7fa26cd34538031b4dba2fc5ba922168230034bd
Pablo <pablo.seban@univ-tlse2.fr>
2020-05-04 19:03:14 +0200

    no debug toolbar

--- FlOpEDT/FlOpEDT/settings/local.py
+++ FlOpEDT/FlOpEDT/settings/local.py
@@ -75,8 +75,8 @@ CHANNEL_LAYERS = {
     },
 }
 
-INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
+# INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
 
-MIDDLEWARE = MIDDLEWARE + ['debug_toolbar.middleware.DebugToolbarMiddleware']
+# MIDDLEWARE = MIDDLEWARE + ['debug_toolbar.middleware.DebugToolbarMiddleware']
 
 INTERNAL_IPS = ['127.0.0.1']
