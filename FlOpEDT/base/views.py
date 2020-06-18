dc0ef11e545325c1c0a1e5633594c98b5a48b6e4
Pablo <pablo.seban@univ-tlse2.fr>
2020-06-18 09:32:19 +0200

    fetch all dispos in json

--- FlOpEDT/base/views.py
+++ FlOpEDT/base/views.py
@@ -611,7 +611,7 @@ def fetch_dispos(req, year, week, **kwargs):
 @dept_admin_required
 def fetch_all_dispos(req, **kwargs):
     dataset = AllDispoResource().export(UserPreference.objects.all())
-    return HttpResponse(dataset.csv,content_type='text/csv')
+    return HttpResponse(dataset.json, content_type='text/json')
 
 
 def fetch_course_default_week(req, train_prog, course_type, **kwargs):
