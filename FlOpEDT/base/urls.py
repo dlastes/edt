b15303bca1040773d07207b91560970e594bbcfd
PRG <prenaud@irit.fr>
2020-09-23 20:20:35 +0200

    minor: 80 char

--- FlOpEDT/base/urls.py
+++ FlOpEDT/base/urls.py
@@ -99,14 +99,23 @@ urlpatterns = [
 
     # from screen to db
     path('change_edt', views.edt_changes, name="edt_changes"),
-    path('change_user_pref/<int:year>/<int:week>/<str:username>', views.user_preferences_changes, name="user_pref_changes"),
+    path('change_user_pref/<int:year>/<int:week>/<str:username>',
+         views.user_preferences_changes,
+         name="user_pref_changes"),
     path('change_course_pref/<int:year>/<int:week>/<str:train_prog>/<str:course_type>', views.course_preferences_changes, name="course_pref_changes"),
-    path('change_room_pref/<int:year>/<int:week>/<str:room>', views.room_preferences_changes, name="room_pref_changes"),
+    path('change_room_pref/<int:year>/<int:week>/<str:room>',
+         views.room_preferences_changes,
+         name="room_pref_changes"),
     path('change_decale', views.decale_changes, name="decale_changes"),
-    path('change_perfect_day/<str:username>', views.user_perfect_day_changes, name="user_perfect_day_changes"),
-    path('change_user_notifications_pref/<str:username>', views.user_notifications_pref_changes,
+    path('change_perfect_day/<str:username>',
+         views.user_perfect_day_changes,
+         name="user_perfect_day_changes"),
+    path('change_user_notifications_pref/<str:username>',
+         views.user_notifications_pref_changes,
          name="user_notifications_pref_changes"),
-    path(r'change_room_pref_per_tutor/<str:tutor>', views.room_preferences_changes_per_tutor, name='room_pref_changes_per_tutor'),
+    path(r'change_room_pref_per_tutor/<str:tutor>',
+         views.room_preferences_changes_per_tutor,
+         name='room_pref_changes_per_tutor'),
 
     # predefined
     # ------------
