ff3c31a95e49a29c62020a70f1c6c52c41602631
Pablo <pablo.seban@univ-tlse2.fr>
2021-02-26 13:35:07 +0100

    ics regen ok

--- FlOpEDT/base/models.py
+++ FlOpEDT/base/models.py
@@ -745,10 +745,10 @@ class Regen(models.Model):
     def strplus(self):
         ret = ""
         if self.full:
-            ret += f'Génération complète le ' + \
+            ret += f'Re-génération complète prévue le ' + \
                    f'{self.fday}/{self.fmonth}/{self.fyear}'
         elif self.stabilize:
-            ret += 'Génération stabilisée le ' + \
+            ret += 'Génération stabilisée prévue le ' + \
                    f'{self.sday}/{self.smonth}/{self.syear}'
         else:
             ret += "Pas de (re-)génération prévue"
