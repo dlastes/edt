82206c79f73e288098163dc843867dfea1a5212b
PRG <prenaud@irit.fr>
2021-03-13 15:34:27 +0100

    allow multiple contexts in a url

--- FlOpEDT/base/static/base/helpers.js
+++ FlOpEDT/base/static/base/helpers.js
@@ -43,10 +43,16 @@ function get_transition(immediate) {
 
 
 // add get parameters to the url
-function build_url(url, context) {
-  return url + "?" + Object.keys(context).map(
+function build_url(url, ...contexts) {
+  let full_context = contexts.reduce(
+    function(acc, val) {
+      return Object.assign(acc, val) ;
+    },
+    {}
+  );
+  return url + "?" + Object.keys(full_context).map(
     function (p) {
-      return p + "=" + context[p] ;
+      return p + "=" + full_context[p] ;
     }
   ).join("&");
 }
