63ae03eda4be7da5ca816ccc759d7f6b36dc58ec
Pablo <pablo.seban@univ-tlse2.fr>
2021-04-08 13:17:37 +0200

    ENSMM plus complet

--- FlOpEDT/MyFlOp/ENSMM.py
+++ FlOpEDT/MyFlOp/ENSMM.py
@@ -1,4 +1,7 @@
-from base.models import Course, TrainingProgramme, Group, Department
+from base.models import Course, TrainingProgramme, Group, Department, \
+    TimeGeneralSettings, CourseType, RoomType, CourseStartTimeConstraint
+from people.models import Tutor
+from people.tutor import fill_default_user_preferences
 from configuration.deploy_database import extract_database_file
 from MyFlOp.database_ENSMM import database_ENSMM
 from MyFlOp.cours_ENSMM import cours_ENSMM
@@ -6,15 +9,23 @@ from configuration.hyperplanning import extract_courses_from_book
 from misc.assign_colors import assign_module_color
 
 
-def global_extraction(abbrev='ENSMM', name='ENSMM', delete_groups=False):
-    extract_database_file(abbrev, name, database_ENSMM)
+def global_extraction(abbrev='ENSMM', name='ENSMM', delete_groups=True):
+    extract_database_file(abbrev, name, book=database_ENSMM)
     dep = Department.objects.get(abbrev=abbrev)
+    optimize_settings(dep)
+    for t in Tutor.objects.filter(departments=dep):
+        fill_default_user_preferences(t, dep)
     extract_courses_from_book(cours_ENSMM, dep, assign_colors=False)
+    C = Course.objects.filter(type__department=dep)
+    for c in C:
+        c.year += 1
+        c.save()
     apply_group_architecture(find_group_architecture(dep))
     if delete_groups:
+        remove_slash_groups(dep)
         g, tp = useless_groups_and_train_progs(dep)
         delete_useless_groups(g, tp)
-    assign_module_color(dep)
+    assign_module_color(dep, overwrite=True)
 
 
 def find_group_architecture(dep):
@@ -67,3 +78,35 @@ def delete_useless_groups(useless_groups, useless_train_progs):
         g.delete()
     for tp in useless_train_progs:
         tp.delete()
+
+
+def remove_slash_groups(dep):
+    slash_groups = Group.objects.filter(name__contains='/', train_prog__department=dep)
+    for g in slash_groups:
+        names = g.name.split('/')
+        groups = Group.objects.filter(name__in=names)
+        for c in Course.objects.filter(groups=g):
+            c.groups.remove(g)
+            for group in groups:
+                c.groups.add(group)
+        g.train_prog.delete()
+
+
+def optimize_settings(dep):
+    tgs = TimeGeneralSettings.objects.get(department=dep)
+    tgs.days = ['m', 'tu', 'w', 'th', 'f']
+    tgs.default_preference_duration = 120
+    tgs.save()
+    CST = CourseStartTimeConstraint.objects.filter(course_type__department=dep)
+    for cst in CST:
+        l = [st for st in cst.allowed_start_times if st % 30 == 0]
+        if l:
+            cst.allowed_start_times = l
+            cst.save()
+    RT, created = RoomType.objects.get_or_create(name='None', department=dep)
+    if created:
+        for i, c in enumerate(Course.objects.filter(type__department=dep)):
+            if i % 10 != 0:
+                c.room_type = RT
+                c.save()
+
