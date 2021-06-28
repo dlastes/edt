91ae2dc4628f379cfc584f3514815db8ca41abe1
Pablo <pablo.seban@univ-tlse2.fr>
2021-06-28 15:52:31 +0200

    debug edt_changes

--- FlOpEDT/base/views.py
+++ FlOpEDT/base/views.py
@@ -936,7 +936,7 @@ def edt_changes(req, **kwargs):
     try:
         week = json.loads(req.POST.get('week'))
         year = json.loads(req.POST.get('year'))
-        week = Week.objects.get(week=week, year=year)
+        week = Week.objects.get(nb=week, year=year)
         work_copy = json.loads(req.POST.get('work_copy'))
         old_version = json.loads(req.POST.get('version'))
         department = req.department
@@ -984,7 +984,7 @@ def edt_changes(req, **kwargs):
                 return JsonResponse(bad_response)
 
             if work_copy == 0:
-                edt_version = EdtVersion.objects.get(week=week, year=year, department=department)
+                edt_version = EdtVersion.objects.get(week=week, department=department)
                 edt_version.version += 1
                 edt_version.save()
 
