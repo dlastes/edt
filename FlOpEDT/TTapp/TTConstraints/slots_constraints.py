00135404ba76c68c8e28ac89c48ba09fe905aace
csl0779a <leo.cusseau@univ-tlse3.fr>
2021-07-19 19:17:30 +0200

    migrating Precedence to ConsiderDependencies, no tests

--- FlOpEDT/TTapp/TTConstraints/slots_constraints.py
+++ FlOpEDT/TTapp/TTConstraints/slots_constraints.py
@@ -24,16 +24,20 @@
 # without disclosing the source code of your own applications.
 
 
+from datetime import timedelta
+
+from django.http.response import JsonResponse
+from TTapp.TTConstraints.no_course_constraints import NoTutorCourseOnDay
+from base.models import CourseStartTimeConstraint, Dependency, ModulePossibleTutors, UserPreference
 from django.contrib.postgres.fields import ArrayField
 
 from django.db import models
-
-from base.timing import french_format
+from django.db.models import Q
+from base.timing import TimeInterval, flopdate_to_datetime, french_format, Day
 
 from TTapp.ilp_constraints.constraint_type import ConstraintType
 from TTapp.ilp_constraints.constraint import Constraint
 from TTapp.slots import days_filter, slots_filter
-from base.timing import Day
 from TTapp.TTConstraint import TTConstraint
 from TTapp.ilp_constraints.constraints.dependencyConstraint import DependencyConstraint
 
@@ -178,7 +182,43 @@ class LimitedStartTimeChoices(TTConstraint):
             text += french_format(pst) + ', '
         return text
 
+### ConsiderDepencies dependencies
+def find_successive_slots(course_slot1, course_slot2, course1_duration, course2_duration):
+    for cs1 in course_slot1:
+        possible_start_time = cs1.start + course1_duration
+        for cs2 in course_slot2:
+            if cs2.start <= possible_start_time and cs2.end > possible_start_time + course2_duration:
+                return True
+            if cs2.start > possible_start_time:
+                break
+    return False
 
+def tutor_and_supp(interval, required_supp, possible_tutors):
+    supp_in = False
+    tutor_in = False
+    if "user_preference" in interval[1]:
+        for tutor, value in interval[1]["user_preference"].items():
+            if tutor in required_supp and value > 0:
+                supp_in = (supp_in
+                    or "no_course_tutor" not in interval[1]
+                    or tutor not in interval[1]["no_course_tutor"]["tutors"]
+                        and tutor.status not in interval[1]["no_course_tutor"]["tutor_status"])
+            elif tutor in possible_tutors and value > 0:
+                tutor_in = (tutor_in
+                    or "no_course_tutor" not in interval[1]
+                    or tutor not in interval[1]["no_course_tutor"]["tutors"]
+                        and tutor.status not in interval[1]["no_course_tutor"]["tutor_status"])
+            if supp_in and tutor_in:
+                break
+    return supp_in and tutor_in
+
+
+def find_day_gap_slots(course_slot1, course_slot2, day_gap):
+    day_slot = course_slot1[0] + timedelta(days=day_gap) - timedelta(hours=course_slot1[0].hour, minutes=course_slot1[0].minute)
+    for cs2 in course_slot2:
+        if cs2.start > day_slot:
+            return True
+    return False
 
 class ConsiderDepencies(TTConstraint):
     """
@@ -189,6 +229,208 @@ class ConsiderDepencies(TTConstraint):
     If there is a weight, it's a preference, else it's a constraint...
     """
     modules = models.ManyToManyField('base.Module', blank=True)
+
+    def pre_analyse(self, week):
+        dependencies = self.considered_dependecies().filter(Q(course1__week=week) | Q(course1__week=None), Q(course2__week=week) | Q(course2__week=None))
+        jsondict = {"status" : "OK", "messages" : []}   
+        print(dependencies)
+        for dependency in dependencies:
+            # Setting up empty partitions for both courses
+            week_partition_course1 = self.get_partition_of_week(week, with_day_time=True)
+            week_partition_course2 = self.get_partition_of_week(week, with_day_time=True)
+
+            # Retrieving possible start times for both courses
+            course1_start_times = CourseStartTimeConstraint.objects.get(course_type = dependency.course1.type).allowed_start_times
+            course2_start_times = CourseStartTimeConstraint.objects.get(course_type = dependency.course2.type).allowed_start_times
+
+            # Setting up possible tutors and required ones (or supp_tutors)
+            possible_tutors_1 = set()
+            required_supp_1 = set()
+            if dependency.course1.tutor is not None:
+                possible_tutors_1.add(dependency.course1.tutor)
+            elif ModulePossibleTutors.objects.filter(module = dependency.course1.module).exists():
+                possible_tutors_1 = set(ModulePossibleTutors.objects.get(module = dependency.course1.module).possible_tutors.all())
+            else:
+                mods_possible_tutor = ModulePossibleTutors.objects.filter(module__train_prog__department = self.department)
+                possible_tutors_1 = set(mod.possible_tutors.all() for mod in mods_possible_tutor)
+
+            if dependency.course1.supp_tutor is not None:
+                required_supp_1 = set(dependency.course1.supp_tutor.all())
+
+            possible_tutors_2 = set()
+            required_supp_2 = set()
+            if dependency.course2.tutor is not None:
+                possible_tutors_2.add(dependency.course2.tutor)
+            elif ModulePossibleTutors.objects.filter(module = dependency.course2.module).exists():
+                possible_tutors_2 = set(ModulePossibleTutors.objects.get(module = dependency.course2.module).possible_tutors.all())
+            else:
+                mods_possible_tutor = ModulePossibleTutors.objects.filter(module__train_prog__department = self.department)
+                possible_tutors_1 = set(mod.possible_tutors.all() for mod in mods_possible_tutor)
+
+            if dependency.course2.supp_tutor is not None:
+                required_supp_2 = set(dependency.course2.supp_tutor.all())
+
+            # Retrieving UserPreferences for possible tutors
+            D1 = UserPreference.objects.filter(user__in=possible_tutors_1, week=week, value__gte=1)
+            if not D1:
+                D1 = UserPreference.objects.filter(user__in=possible_tutors_1, week=None, value__gte=1)
+
+            D2 = UserPreference.objects.filter(user__in=possible_tutors_2, week=week, value__gte=1)
+            if not D2:
+                D2 = UserPreference.objects.filter(user__in=possible_tutors_2, week=None, value__gte=1)
+            
+            if D1 and D2:
+
+                # Retrieving constraints for days were required tutors shouldn't be working
+                no_course_tutor1 = (NoTutorCourseOnDay.objects
+                    .filter(Q(tutors__in = required_supp_1.union(possible_tutors_1))
+                        | Q(tutor_status = [pt.status for pt in required_supp_1.union(possible_tutors_1)]),
+                        weeks = week))
+                if not no_course_tutor1:
+                    no_course_tutor1 = (NoTutorCourseOnDay.objects
+                    .filter(Q(tutors__in = required_supp_1.union(possible_tutors_1))
+                        | Q(tutor_status = [pt.status for pt in required_supp_1.union(possible_tutors_1)]),
+                        weeks = None))
+                
+                no_course_tutor2 = (NoTutorCourseOnDay.objects
+                    .filter(Q(tutors__in = required_supp_2.union(possible_tutors_2))
+                        | Q(tutor_status = [pt.status for pt in required_supp_2.union(possible_tutors_2)]),
+                        weeks = week))
+                if not no_course_tutor2:
+                    no_course_tutor2 = (NoTutorCourseOnDay.objects
+                    .filter(Q(tutors__in = required_supp_2.union(possible_tutors_2))
+                        | Q(tutor_status = [pt.status for pt in required_supp_2.union(possible_tutors_2)]),
+                        weeks = None))
+                # Adding all user preferences to the partition
+                for up in D1:
+                    up_day = Day(up.day, week)
+                    week_partition_course1.add_slot(
+                        TimeInterval(flopdate_to_datetime(up_day, up.start_time),
+                        flopdate_to_datetime(up_day, up.end_time)),
+                        "user_preference",
+                        {"value" : up.value, "available" : True, "tutor" : up.user}
+                    )
+                for up in D2:
+                    up_day = Day(up.day, week)
+                    week_partition_course2.add_slot(
+                        TimeInterval(flopdate_to_datetime(up_day, up.start_time),
+                        flopdate_to_datetime(up_day, up.end_time)),
+                        "user_preference",
+                        {"value" : up.value, "available" : True, "tutor" : up.user}
+                    )
+
+                for constraint in no_course_tutor1:
+                    slot = constraint.get_slot_constraint(week)
+                    if slot:
+                        week_partition_course1.add_slot(
+                            slot[0],
+                            "no_course_tutor",
+                            slot[1]
+                        )
+                for constraint in no_course_tutor2:
+                    slot = constraint.get_slot_constraint(week)
+                    if slot:
+                        week_partition_course2.add_slot(
+                            slot[0],
+                            "no_course_tutor",
+                            slot[1]
+                        )
+
+                if required_supp_1:
+                    #Retrieving and adding user preferences for the required tutors
+                    RUS1 = UserPreference.objects.filter(user__in=required_supp_1, week=week, value__gte=1)
+                    if not RUS1:
+                        RUS1 = UserPreference.objects.filter(user__in=required_supp_1, week=None, value__gte=1)
+
+                    for up in RUS1:
+                        up_day = Day(up.day, week)
+                        week_partition_course1.add_slot(
+                            TimeInterval(flopdate_to_datetime(up_day, up.start_time),
+                            flopdate_to_datetime(up_day, up.end_time)),
+                            "user_preference",
+                            {"value" : up.value, "available" : True, "tutor" : up.user}
+                        )
+
+                    for interval in week_partition_course1.intervals:
+                        if not tutor_and_supp(interval, required_supp_1, possible_tutors_1):
+                            interval[1]["available"] = False
+
+
+
+                if required_supp_2:
+                    #Retrieving and adding user preferences for the required tutors
+                    for interval in week_partition_course2.intervals:
+                        if tutor_and_supp(interval, required_supp_2, possible_tutors_2):
+                            interval[1]["available"] = False
+                    RUS2 = UserPreference.objects.filter(user__in=required_supp_2, week=week, value__gte=1)
+                    if not RUS2:
+                        RUS2 = UserPreference.objects.filter(user__in=required_supp_2, week=None, value__gte=1)
+                    for up in RUS2:
+                        up_day = Day(up.day, week)
+                        week_partition_course2.add_slot(
+                            TimeInterval(flopdate_to_datetime(up_day, up.start_time),
+                            flopdate_to_datetime(up_day, up.end_time)),
+                            "user_preference",
+                            {"value" : up.value, "available" : True, "tutor" : up.user}
+                        )
+
+
+
+                course1_slots = week_partition_course1.find_all_available_timeinterval_with_key_starting_at("user_preference", course1_start_times, dependency.course1.type.duration)
+                course2_slots = week_partition_course2.find_all_available_timeinterval_with_key_starting_at("user_preference", course2_start_times, dependency.course2.type.duration)
+
+                if course1_slots and course2_slots:
+                    while course2_slots[0].end < course1_slots[0].start + timedelta(hours = dependency.course1.type.duration/60, minutes=dependency.course1.type.duration%60):
+                        course2_slots.pop(0)
+                        if not course2_slots:
+                            break
+                    if course2_slots:
+                        if course1_slots[0].start + timedelta(hours = dependency.course1.type.duration/60, minutes=dependency.course1.type.duration%60) > course2_slots[0].start:
+                            course2_slots[0].start = course1_slots[0].start + timedelta(hours = dependency.course1.type.duration/60, minutes=dependency.course1.type.duration%60)
+                        # Here we check if the first course_slot that we might just shrank is still long enough and if it is the only
+                        # one left.
+                        if len(course2_slots) <= 1 and course2_slots[0].duration < dependency.course2.type.duration:
+                            jsondict["status"] = "KO"
+                            jsondict["messages"].append(_('There is no available slots for the second course after the first one.'))
+                    else:
+                        jsondict["status"] = "KO"
+                        jsondict["messages"].append(_('There is no available slots for the second course.'))
+                else:
+                    jsondict['status'] = "KO"
+                    jsondict["messages"].append(_('There is no available slots for the first or the second course.'))
+
+                if jsondict["status"] == "OK":
+                    if dependency.successive:
+                        if not find_successive_slots(
+                            course1_slots,
+                            course2_slots,
+                            timedelta(hours = dependency.course1.type.duration/60, minutes = dependency.course1.type.duration%60),
+                            timedelta(hours = dependency.course2.type.duration/60, minutes = dependency.course2.type.duration%60)):
+                            jsondict['status'] = "KO"
+                            jsondict["messages"].append(_('There is no available successive slots for those courses.'))
+                    if dependency.day_gap != 0:
+                        if not find_day_gap_slots(course1_slots, course2_slots, dependency.day_gap):
+                            jsondict['status'] = "KO"
+                            jsondict["messages"].append(_(f'There is no available slots for the second course after a {dependency.day_gap} day gap.'))
+            else:
+                # Some users don't have any availabilities
+                jsondict['status'] = "KO"
+                if not D1:
+                    jsondict['messages'].append(_(f"There is no available tutor for {dependency.course1.full_name()}"))
+                if not D2:
+                    jsondict['messages'].append(_(f"There is no available tutor for {dependency.course2.full_name()}"))
+        return JsonResponse(jsondict)
+
+    def considered_dependecies(self):
+        """Returns the depencies that have to be considered"""
+        result=Dependency.objects.filter(course1__type__department=self.department, course2__type__department=self.department)
+        if self.train_progs.exists():
+            result = result.filter(course1__train_prog__in=self.train_progs.all(), course2__train_prog__in=self.train_progs.all())
+        if self.modules.exists():
+            result = result.filter(course1__module__in=self.modules.all(), course2__module__in=self.modules.all())
+        if self.weeks.exists():
+            result = result.filter(course1__week__in=self.weeks, course2__week__in=self.weeks)
+        return result
 
     def one_line_description(self):
         text = f"Prend en compte les précédences enregistrées en base."
