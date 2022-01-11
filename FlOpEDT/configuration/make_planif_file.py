bc29ad65eec22c5dcff62451aeb94afe81e1d37c
Pablo <pablo.seban@univ-tlse2.fr>
2022-01-11 16:14:32 +0100

    bug in groups make file

--- FlOpEDT/configuration/make_planif_file.py
+++ FlOpEDT/configuration/make_planif_file.py
@@ -214,12 +214,20 @@ def make_planif_file(department, empty_bookname=default_empty_bookname, target_r
                 sheet.cell(row=rank, column=VERIF_COL).value = '=SUM(%s%d:%s%d)' % (first_column_letter[p], rank,
                                                                                     last_column_letter[p], rank)
                 rank += 1
-                if with_courses:
-                    groups = set(StructuralGroup.objects.filter(train_prog=mod.train_prog))
-                             #| set(TransversalGroup.objects.filter(train_prog=mod.train_prog))
-                else:
-                    groups = set(StructuralGroup.objects.filter(train_prog=mod.train_prog))
-                             #| set(TransversalGroup.objects.filter(type__in=ct.group_types.all(), train_prog=mod.train_prog))
+                groups = set(StructuralGroup.objects.filter(train_prog=mod.train_prog,
+                                                            type__in=ct.group_types.all())) \
+                         | set(TransversalGroup.objects.filter(train_prog=mod.train_prog,
+                                                               type__in=ct.group_types.all()))
+                # if with_courses:
+                #     groups = set(StructuralGroup.objects.filter(train_prog=mod.train_prog,
+                #                                                 type__in=ct.group_types.all())) \
+                #              | set(TransversalGroup.objects.filter(train_prog=mod.train_prog,
+                #                                                    type__in=ct.group_types.all()))
+                # else:
+                #     groups = set(StructuralGroup.objects.filter(train_prog=mod.train_prog,
+                #                                                 type__in=ct.group_types.all())) \
+                #              | set(TransversalGroup.objects.filter(train_prog=mod.train_prog,
+                #                                                    type__in=ct.group_types.all()))
 
                 nb_groups = len(groups)
                 if nb_groups:
