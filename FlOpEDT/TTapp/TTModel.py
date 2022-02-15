f19ed5c03b6766e4a0673ca9f0b76b24d63c991c
Pablo <pablo.seban@univ-tlse2.fr>
2022-02-15 11:47:23 +0100

    FlopModel

--- FlOpEDT/TTapp/TTModel.py
+++ FlOpEDT/TTapp/TTModel.py
@@ -33,13 +33,8 @@ from pulp import LpVariable, LpConstraint, LpBinary, LpConstraintEQ, \
 import pulp
 from pulp import GUROBI_CMD
 
-from base.models import StructuralGroup, \
-    Room, RoomSort, RoomType, RoomPreference, \
-    Course, ScheduledCourse, UserPreference, CoursePreference, \
-    Department, Module, TrainingProgramme, CourseType, \
-    Dependency, TutorCost, GroupFreeHalfDay, GroupCost, Holiday, TrainingHalfDay, \
-    CourseStartTimeConstraint, TimeGeneralSettings, ModulePossibleTutors, CoursePossibleTutors, \
-    ModuleTutorRepartition, ScheduledCourseAdditional
+from base.models import RoomType, RoomPreference, ScheduledCourse, Department, TrainingProgramme, \
+    TutorCost, GroupFreeHalfDay, GroupCost, TimeGeneralSettings, ModuleTutorRepartition, ScheduledCourseAdditional
 
 from base.timing import Time
 
@@ -54,13 +49,10 @@ from TTapp.slots import slots_filter, days_filter
 
 from TTapp.weeks_database import WeeksDatabase
 
-from MyFlOp.MyTTUtils import reassign_rooms
-
 import signal
 
 from django.db import close_old_connections
 from django.db.models import Q, Max, F
-from django.conf import settings
 
 import datetime
 
@@ -71,13 +63,11 @@ from TTapp.ilp_constraints.constraint import Constraint
 from TTapp.ilp_constraints.constraint_type import ConstraintType
 from TTapp.ilp_constraints.constraints.courseConstraint import CourseConstraint
 
-
-from TTapp.ilp_constraints.constraints.dependencyConstraint import DependencyConstraint
-from TTapp.ilp_constraints.constraints.instructorConstraint import InstructorConstraint
-from TTapp.ilp_constraints.constraints.simulSlotGroupConstraint import SimulSlotGroupConstraint
 from TTapp.ilp_constraints.constraints.slotInstructorConstraint import SlotInstructorConstraint
 
 from FlOpEDT.decorators import timer
+
+from TTapp.FlopModel import FlopModel
 
 logger = logging.getLogger(__name__)
 pattern = r".+: (.|\s)+ (=|>=|<=) \d*"
@@ -86,7 +76,7 @@ GUROBI_NAME = 'GUROBI_CMD'
 solution_files_path = "misc/logs/solutions"
 
 
-class TTModel(object):
+class TTModel(FlopModel):
     @timer
     def __init__(self, department_abbrev, weeks,
                  train_prog=None,
@@ -107,9 +97,8 @@ class TTModel(object):
         # beg_file = os.path.join('logs',"FlOpTT")
         self.department = Department.objects.get(abbrev=department_abbrev)
         self.weeks = weeks
-
+        super(TTModel, self).__init__(keep_many_solution_files=keep_many_solution_files )
         # Create the PuLP model, giving the name of the lp file
-        self.model = LpProblem(self.solution_files_prefix(), LpMinimize)
         self.min_ups_i = min_nps_i
         self.min_bhd_g = min_bhd_g
         self.min_bd_i = min_bd_i
@@ -120,16 +109,12 @@ class TTModel(object):
         self.core_only = core_only
         self.send_mails = send_mails
         self.slots_step = slots_step
-        self.keep_many_solution_files = keep_many_solution_files
         self.min_visio = min_visio
-        self.var_nb = 0
-        self.constraintManager = ConstraintManager()
         self.with_rooms = with_rooms
 
         print("\nLet's start weeks #%s" % weeks)
 
         print("Initialisation...")
-        self.warnings = {}
 
         if train_prog is None:
             train_prog = TrainingProgramme.objects.filter(department=self.department)
@@ -141,7 +126,6 @@ class TTModel(object):
             print('Will modify only courses of training programme(s) ', train_prog)
         self.train_prog = train_prog
         self.stabilize_work_copy = stabilize_work_copy
-        self.obj = self.lin_expr()
         self.wdb = self.wdb_init()
         if not self.wdb.courses.exists():
             print('There are no course to be scheduled...')
@@ -160,8 +144,6 @@ class TTModel(object):
         self.unp_slot_cost_course, self.avail_course \
             = self.compute_non_preferred_slots_cost_course()
         self.avail_room = self.compute_avail_room()
-        self.one_var = self.add_var()
-        self.add_constraint(self.one_var, '==', 1, Constraint(constraint_type=ConstraintType.TECHNICAL))
 
         # Hack : permet que ça marche même si les dispos sur la base sont pas complètes
         for i in self.wdb.instructors:
@@ -372,107 +354,6 @@ class TTModel(object):
 
         return physical_presence, has_visio
 
-    def add_var(self, name=''):
-        """
-        Create a PuLP binary variable
-        """
-        # return LpVariable(name, lowBound = 0, upBound = 1, cat = LpBinary)
-        # countedname = name + '_' + str(self.var_nb)
-        self.var_nb += 1
-
-        # return LpVariable(countedname, cat=LpBinary)
-        return LpVariable(str(self.var_nb), cat=LpBinary)
-
-    def add_constraint(self, expr, relation, value, constraint=Constraint()):
-        constraint_id = self.constraintManager.get_nb_constraints()
-
-        # Add mathematic constraint
-        if relation == '==':
-            pulp_relation = LpConstraintEQ
-        elif relation == '<=':
-            pulp_relation = LpConstraintLE
-        elif relation == '>=':
-            pulp_relation = LpConstraintGE
-        else:
-            raise Exception("relation must be either '==' or '>=' or '<='")
-        self.model += LpConstraint(e=expr, sense=pulp_relation,
-                                   rhs=value, name=str(constraint_id))
-
-        # Add intelligible constraint
-        constraint.id = constraint_id
-        self.constraintManager.add_constraint(constraint)
-
-    def lin_expr(self, expr=None):
-        return LpAffineExpression(expr)
-
-    def sum(self, *args):
-        return lpSum(list(*args))
-
-    def get_var_value(self, ttvar):
-        return round(ttvar.value())
-
-    def get_expr_value(self, ttexpr):
-        return ttexpr.value()
-
-    def get_obj_coeffs(self):
-        """
-        get the coeff of each var in the objective
-        """
-        l = [(weight, var) for (var, weight) in self.obj.items()
-             if var.value() != 0 and round(weight) != 0]
-        l.sort(reverse=True)
-        return l
-
-    def set_objective(self, obj):
-        self.model.setObjective(obj)
-
-    def get_constraint(self, name):
-        return self.model.constraints[name]
-
-    def get_all_constraints(self):
-        return self.model.constraints
-
-    def remove_constraint(self, constraint_name):
-        del self.model.constraints[constraint_name]
-
-    def var_coeff(self, var, constraint):
-        return constraint[var]
-
-    def change_var_coeff(self, var, constraint, newvalue):
-        constraint[var] = newvalue
-
-    def add_conjunct(self, v1, v2):
-        """
-        Crée une nouvelle variable qui est la conjonction des deux
-        et l'ajoute au modèle
-        """
-        l_conj_var = self.add_var("%s AND %s" % (str(v1), str(v2)))
-        self.add_constraint(l_conj_var - (v1 + v2), '>=', -1,
-                            Constraint(constraint_type=ConstraintType.CONJONCTION))
-        self.add_constraint(2 * l_conj_var - (v1 + v2), '<=', 0,
-                            Constraint(constraint_type=ConstraintType.CONJONCTION))
-        return l_conj_var
-
-    def add_floor(self, expr, floor, bound):
-        """
-        Add a variable that equals 1 if expr >= floor, if integer expr is
-        known to be within [0, bound]
-        """
-        l_floor = self.add_var()
-        self.add_constraint(expr - l_floor * floor, '>=', 0,
-                            Constraint(constraint_type=ConstraintType.SEUIL))
-        self.add_constraint(l_floor * bound - expr, '>=', 1 - floor,
-                            Constraint(constraint_type=ConstraintType.SEUIL))
-        return l_floor
-
-    def add_if_var_a_then_not_vars_b_constraint(self, var_a, vars_b_list, name_of_b_list=None):
-        bound = len(vars_b_list) + 1
-        if name_of_b_list is None:
-            name_of_b_list = "anonymous list"
-        # , 'If %s then not any of %s_%g' % (var_a, name_of_b_list, self.constraint_nb)
-        self.add_constraint(bound * var_a + self.sum(var for var in vars_b_list), '<=', bound,
-                            Constraint(constraint_type=ConstraintType.SI_A_ALORS_NON_B))
-
     def add_to_slot_cost(self, slot, cost):
         self.cost_SL[slot] += cost
 
@@ -484,12 +365,6 @@ class TTModel(object):
 
     def add_to_generic_cost(self, cost, week=None):
         self.generic_cost[week] += cost
-
-    def add_warning(self, key, warning):
-        if key in self.warnings:
-            self.warnings[key].append(warning)
-        else:
-            self.warnings[key] = [warning]
 
     @timer
     def add_stabilization_constraints(self):
@@ -1133,39 +1008,6 @@ class TTModel(object):
     def solution_files_prefix(self):
         return f"flopmodel_{self.department.abbrev}_{'_'.join(str(w) for w in self.weeks)}"
 
-    def all_counted_solution_files(self):
-        solution_file_pattern = f"{self.solution_files_prefix()}_*.sol"
-        result = []
-        for root, dirs, files in os.walk(solution_files_path):
-            for name in files:
-                if fnmatch.fnmatch(name, solution_file_pattern):
-                    result.append(os.path.join(root, name))
-        result.sort(key=lambda filename: int(filename.split('_')[-1].split('.')[0]))
-        return result
-
-    def last_counted_solution_filename(self):
-        return self.all_counted_solution_files()[-1]
-
-    def delete_solution_files(self, all=False):
-        solution_files = self.all_counted_solution_files()
-        if solution_files:
-            for f in solution_files[:-1]:
-                os.remove(f)
-            if all:
-                os.remove(solution_files[-1])
-
-    @staticmethod
-    def read_solution_file(filename):
-        one_vars = set()
-        with open(filename) as f:
-            lines = f.readlines()
-            print(lines[1])
-            for line in lines[2:]:
-                r = line.strip().split(" ")
-                if int(r[1]) == 1:
-                    one_vars.add(r[0])
-        return one_vars
-
     def add_tt_to_db_from_file(self, filename=None, target_work_copy=None):
         if filename is None:
             filename = self.last_counted_solution_filename()
@@ -1235,51 +1077,6 @@ class TTModel(object):
             m.write(iis_filename)
         if write_analysis:
             self.constraintManager.handle_reduced_result(iis_filename, file_path, filename_suffixe)
-
-    def optimize(self, time_limit, solver, presolve=2, threads=None):
-        # The solver value shall be one of the available
-        # solver corresponding pulp command or contain
-        # gurobi
-        if 'gurobi' in solver.lower() and hasattr(pulp, GUROBI_NAME):
-            # ignore SIGINT while solver is running
-            # => SIGINT is still delivered to the solver, which is what we want
-            self.delete_solution_files(all=True)
-            signal.signal(signal.SIGINT, signal.SIG_IGN)
-            solver = GUROBI_NAME
-            options = [("Presolve", presolve),
-                       ("MIPGapAbs", 0.2)]
-            if time_limit is not None:
-                options.append(("TimeLimit", time_limit))
-            if threads is not None:
-                options.append(("Threads",threads))
-            if self.keep_many_solution_files:
-                options.append(('SolFiles',
-                                f"{solution_files_path}/{self.solution_files_prefix()}"))
-            result = self.model.solve(GUROBI_CMD(keepFiles=1,
-                                                 msg=True,
-                                                 options=options))
-            if result is None or result == 0:
-                self.write_infaisability()
-
-        elif hasattr(pulp, solver):
-            # raise an exception when the solver name is incorrect
-            command = getattr(pulp, solver)
-            self.model.solve(command(keepFiles=1,
-                                     msg=True,
-                                     presolve=presolve,
-                                     maxSeconds=time_limit))
-        else:
-            print(f'Solver {solver} not found.')
-            return None
-
-        status = self.model.status
-        print(LpStatus[status])
-        if status == LpStatusOptimal or (solver != GUROBI_NAME and status == LpStatusNotSolved):
-            return self.get_obj_coeffs()
-
-        else:
-            print(f'lpfile has been saved in {self.solution_files_prefix()}-pulp.lp')
-            return None
 
     def solve(self, time_limit=None, target_work_copy=None, solver=GUROBI_NAME, threads=None):
         """
