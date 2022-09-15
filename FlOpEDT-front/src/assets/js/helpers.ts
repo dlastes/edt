f4699c3e42642c93cebb476a8d1de5e01d75f4b9
David Gond <david.gond.coding@gmail.com>
2022-09-15 14:01:23 +0200

    fix(helpers): Use Math.round to get human time minutes to compensate float precision issues

--- FlOpEDT-front/src/assets/js/helpers.ts
+++ FlOpEDT-front/src/assets/js/helpers.ts
@@ -1,5 +1,5 @@
 export function convertDecimalTimeToHuman (time: number): string {
   let hours = Math.trunc(time)
-  let minutes = Math.trunc((time - hours) * 60)
-  return `${hours}h${minutes > 0 ? minutes : '00'}`
+  let minutes = Math.round((time - hours) * 60)
+  return `${hours}h${minutes > 0 ? (minutes < 10 ? `0${minutes}` : minutes) : '00'}`
 }
