ff4305e8dd136877181022ae8f9102cedd690b4d
Pablo <pablo.seban@univ-tlse2.fr>
2022-10-05 17:47:38 +0200

    roomReservation admin

---
+++ FlOpEDT/roomreservation/admin.py
@@ -0,0 +1,16 @@
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
+
+from django.contrib import admin
+
+from base.admin import MyModelAdmin
+
+from roomreservation.models import RoomReservationType
+
+
+class RoomReservationTypeAdmin(MyModelAdmin):
+    list_display = ('name', 'color_bg')
+    ordering = ('name',)
+
+
+admin.site.register(RoomReservationType, RoomReservationTypeAdmin)
