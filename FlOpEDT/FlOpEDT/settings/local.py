b8285c01b2a7fa9151a01a7c31b0f7d15ce738e7
David Gond <david.gond.coding@gmail.com>
2022-10-19 10:54:01 +0200

    refactor: Replace database name and migrations

--- FlOpEDT/FlOpEDT/settings/local.py
+++ FlOpEDT/FlOpEDT/settings/local.py
@@ -30,7 +30,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'flop_database_reservation',
+        'NAME': 'flop_database_public_dev',
         'USER': 'flop_user',
         'PASSWORD': 'your_password',
         'HOST': '127.0.0.1',
