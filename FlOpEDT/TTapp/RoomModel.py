f8d87d5461f3b4233101f54da78033ff588fe278
Pablo <pablo.seban@univ-tlse2.fr>
2022-10-20 10:59:56 +0200

    RoomReservations in RoomModel

--- FlOpEDT/TTapp/RoomModel.py
+++ FlOpEDT/TTapp/RoomModel.py
@@ -43,6 +43,10 @@ from TTapp.RoomConstraints.RoomConstraint import LocateAllCourses, \
     LimitGroupMoves, LimitTutorMoves, ConsiderRoomSorts
 from TTapp.FlopConstraint import max_weight
 
+from base.timing import  flopday_to_date, floptime_to_time
+
+from roomreservation.models import RoomReservation
+
 
 class RoomModel(FlopModel):
     @timer
@@ -237,6 +241,12 @@ class RoomModel(FlopModel):
                         day=sl.day.day,
                         week=sl.day.week,
                         room=room, value=0).exists():
+                    avail_room[room][sl] = 0
+                elif RoomReservation.objects.filter(
+                        start_time__lt=floptime_to_time(sl.start_time + sl.duration),
+                        end_time__gt=floptime_to_time(sl.start_time),
+                        date=flopday_to_date(sl.day),
+                        room=room).exists():
                     avail_room[room][sl] = 0
                 else:
                     avail_room[room][sl] = 1
