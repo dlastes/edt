965d7cd7af90dda268d7622f35c48a8d17603cc4
PRG <prenaud@irit.fr>
2022-12-02 19:15:10 +0100

    (fix)cm-room and viewset

--- FlOpEDT/api/roomreservation/views.py
+++ FlOpEDT/api/roomreservation/views.py
@@ -42,6 +42,33 @@ class RoomReservationViewSet(viewsets.ModelViewSet):
     filterset_fields = '__all__'
 
 
+@method_decorator(name='list',
+                  decorator=swagger_auto_schema(
+                      manual_parameters=[
+                          # in the filterset
+                          week_param(),
+                          year_param()
+                      ])
+                  )
+class ShortRoomReservationViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
+    serializer_class = serializers.ShortRoomReservationSerializer
+
+    def get_queryset(self):
+        all_res = rm.RoomReservation.objects.all()
+        week = self.request.query_params.get('week', None)
+        year = self.request.query_params.get('year', None)
+
+        if week is not None:
+            all_res = all_res.filter(date__week=week)
+        if year is not None:
+            all_res = all_res.filter(date__year=year)
+
+        return all_res
+
+    filterset_fields = ()
+
+
 class RoomReservationTypeViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = serializers.RoomReservationTypeSerializer
